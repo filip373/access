8e5e7609c58464eb64636b00b7751e700eeccb07
Michał Kaźmierczak <michal.kazmierczak@netguru.co>
2014-09-24 13:21:25 +0200

    [#79432364] add user -> github login mapping

--- app/such_services_wow/actions/sync_teams.rb
+++ app/such_services_wow/actions/sync_teams.rb
@@ -8,8 +8,10 @@ module Actions
     def sync_members
       
       expected_teams.each do |et|
+        members = map_users_to_members(et.members)
+
         gh_team = find_or_create_gh_team(et)
-        gh_api.sync_members(gh_team, et.members)
+        gh_api.sync_members(gh_team, members)
         gh_api.sync_repos(gh_team, et.repos)
       end
     end
@@ -18,6 +20,10 @@ module Actions
 
     def find_or_create_gh_team(expected_team)
       gh_api.get_team(expected_team.name) || gh_api.create_team(expected_team.name)
+    end
+
+    def map_users_to_members(members)
+      members.map { |m| expected_users[m].github }
     end
   end
 end
