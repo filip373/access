5c2d26e73adb82b624e37b8f330e272004b7978f
Michał Kaźmierczak <michal.kazmierczak@netguru.co>
2014-09-24 13:04:55 +0200

    [#79432364] add users loading

---
+++ app/entities/expected_users.rb
@@ -0,0 +1,29 @@
+class ExpectedUsers
+
+  def load!
+    data
+  end
+
+  def all
+    @all ||= load!
+  end
+
+  def data
+    @data ||= begin
+      users = {}
+      Dir.glob("#{permissions_repo_path}/users/*.yml") do |file_path|
+        user_name = File.basename(file_path, '.yml')
+        file_data = YAML.load(File.read(file_path))
+
+        users[user_name] = User.new(user_name, file_data['github'])
+      end
+
+      users
+    end
+  end
+
+  def permissions_repo_path
+    "#{Rails.root}/tmp/permissions"
+  end
+
+end
---
+++ app/entities/user.rb
@@ -0,0 +1,2 @@
+class User < Struct.new(:name, :github)
+end
