074443b79c7dc93c0a4dcdf0c7716f805bd6c840
Michał Kaźmierczak <michal.kazmierczak@netguru.co>
2014-09-24 12:53:44 +0200

    remove trailing spaces

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 GithubApp::Application.routes.draw do
   root 'github#index'
   get '/auth/github/callback' => 'session#create'
-  
+
   resources :github, only: [:index] do
     collection do
       post :do_sync
