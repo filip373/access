dfb0bafa66c682bf377220b38114d7866a47f992
Michał Kaźmierczak <michal.kazmierczak@netguru.co>
2014-09-24 13:05:27 +0200

    [#79432364] expose users

--- app/controllers/github_controller.rb
+++ app/controllers/github_controller.rb
@@ -1,9 +1,10 @@
 class GithubController < ApplicationController
-  expose(:gh_api){  GhApi.new(session[:token], AppConfig.company)  }
+  expose(:gh_api) {  GhApi.new(session[:token], AppConfig.company) }
 
-  expose(:expected_teams){ ExpectedTeams.new.all }
+  expose(:expected_teams) { ExpectedTeams.new.all }
+  expose(:expected_users) { ExpectedUsers.new.all }
 
-  expose(:sync){ Actions::SyncTeams.new(expected_teams, gh_api) }
+  expose(:sync) { Actions::SyncTeams.new(expected_teams, expected_users, gh_api) }
 
 	def do_sync
     sync.now!
