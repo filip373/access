9f2bd99deaecfb495281e8eaaf204d00f3e228ad
Michał Kaźmierczak <michal.kazmierczak@netguru.co>
2014-09-25 23:57:39 +0200

    add slack notifier

---
+++ app/entities/slack_notifier.rb
@@ -0,0 +1,19 @@
+class SlackNotifier < Struct.new(:token, :room, :compare_url, :app_url)
+  def notify_on_change
+    client.ping message
+  end
+
+  def message
+    "Click here: #{compare_url} to review.\nClick here: #{app_url} to apply."
+  end
+
+  def name
+    "Github Access"
+  end
+
+  def client
+    @client ||= Slack::Notifier.new "netguru", token,
+                               channel: room,
+                               username: 'gh-permissions'
+  end
+end
