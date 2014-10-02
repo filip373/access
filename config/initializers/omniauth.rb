c41cb1ed011cea9d0e0861dac0789e0bcf8679b6
Michał Kaźmierczak <michal.kazmierczak@netguru.co>
2014-10-02 10:15:32 +0200

    add github scope

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
 	provider :developer unless Rails.env.production?
-	provider :github, AppConfig.github.client_id, AppConfig.github.client_secret, scope: 'admin:org'
+	provider :github, AppConfig.github.client_id, AppConfig.github.client_secret, scope: 'admin:org,repo'
 end
