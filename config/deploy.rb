8644594f8f7525c91c55584c1d790ab57c6d659a
Marcin Stecki <marcin@netguru.pl>
2014-10-08 17:08:48 +0200

    linked files, linked dirs

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,10 +25,10 @@ set :gateway, ENV['GATEWAY']
 # set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/sec_config.yml}
 
 # Default value for linked_dirs is []
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
