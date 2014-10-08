67e60658decab9d443e9f48c3c2ee160641abd1a
Marcin Stecki <marcin@netguru.pl>
2014-10-08 14:59:21 +0200

    all teams

--- app/controllers/github_controller.rb
+++ app/controllers/github_controller.rb
@@ -1,7 +1,7 @@
 class GithubController < ApplicationController
   expose(:gh_api) { GithubIntegration::Api.new(session[:token], AppConfig.company) }
 
-  expose(:expected_teams) { GithubIntegration::Teams.all[0..4] }
+  expose(:expected_teams) { GithubIntegration::Teams.all }
 
   expose(:update_repo) { UpdateRepo.new }
   expose(:sync_github) { GithubIntegration::Actions::SyncTeams.new(expected_teams, gh_api) }
