1823ec5fe955049b1e1e370d330db42d35212a13
Marcin Stecki <marcin@netguru.pl>
2014-10-13 10:30:48 +0200

    gateway

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,5 @@
 set :application, 'access'
 set :repo_url, 'git://github.com/netguru/access.git'
 set :deploy_to, ENV["DEPLOY_PATH"]
-set :gateway, ENV['GATEWAY']
 set :linked_files, %w{config/sec_config.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,1 +1,2 @@
 server ENV['PRODUCTION_SERVER'], user: ENV['PRODUCTION_USER'], roles: %w{web app db}
+set :ssh_options, proxy: Net::SSH::Proxy::Command.new("ssh #{ENV['GATEWAY']} -W %h:%p')
