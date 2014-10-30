a9431d093d21ad644376f1339f3e04256f47a385
Krzysztof Urban <krzysztof.urban@netguru.pl>
2014-10-30 14:26:56 +0100

    scrub access token from logs

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -3,5 +3,5 @@ Rollbar.configure do |config|
 
   config.access_token = AppConfig.rollbar_token
   config.enabled = !(Rails.env.test? or Rails.env.development?)
-
+  config.scrub_fields |= [:access_token]
 end
