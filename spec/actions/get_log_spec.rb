6ee689297941541780ff60af2bb6f06aaee45bd7
Mateusz Stebnicki <mateusz.stebnicki@netguru.pl>
2014-11-28 16:42:29 +0100

    add without changes specs
    [#80823344]

--- spec/actions/get_log_spec.rb
+++ spec/actions/get_log_spec.rb
@@ -31,17 +31,38 @@ RSpec.describe GithubIntegration::Actions::GetLog do
       }
     }
   end
-  subject { described_class.new(diff).now! }
 
+  let(:empty_diff) do
+    {
+      create_teams: {},
+      add_members: {},
+      remove_members: {},
+      add_repos: {},
+      remove_repos: {},
+      change_permissions: {}
+    }
+  end
+
+  subject { described_class.new(diff).now! }
   it { is_expected.to be_a Array }
-  it { is_expected.to satisfy { |s| s.size == 9 } }
-  it { is_expected.to include "[api] create team #{new_team.name}" }
-  it { is_expected.to include "[api] add member #{diff[:create_teams][new_team][:add_members][0]} to team #{new_team.name}" }
-  it { is_expected.to include "[api] add repo #{diff[:create_teams][new_team][:add_repos][0]} to team #{new_team.name}" }
-  it { is_expected.to include "[api] add permissions #{new_team.name} - #{diff[:create_teams][new_team][:add_permissions]}" }
-  it { is_expected.to include "[api] change permission #{team.name} - #{diff[:change_permissions][team][:permissions]}" }
-  it { is_expected.to include "[api] add member #{diff[:add_members][team][:members][0]} to team #{team.name}" }
-  it { is_expected.to include "[api] remove member #{diff[:remove_members][team][:members][0]} from team #{team.name}" }
-  it { is_expected.to include "[api] add repo #{diff[:add_repos][team][:repos][0]} to team #{team.name}" }
-  it { is_expected.to include "[api] remove repo #{diff[:remove_repos][team][:repos][0]} from team #{team.name}" }
+
+  context 'with changes' do
+    it { is_expected.to satisfy { |s| s.size == 9 } }
+    it { is_expected.to include "[api] create team #{new_team.name}" }
+    it { is_expected.to include "[api] add member #{diff[:create_teams][new_team][:add_members][0]} to team #{new_team.name}" }
+    it { is_expected.to include "[api] add repo #{diff[:create_teams][new_team][:add_repos][0]} to team #{new_team.name}" }
+    it { is_expected.to include "[api] add permissions #{new_team.name} - #{diff[:create_teams][new_team][:add_permissions]}" }
+    it { is_expected.to include "[api] change permission #{team.name} - #{diff[:change_permissions][team][:permissions]}" }
+    it { is_expected.to include "[api] add member #{diff[:add_members][team][:members][0]} to team #{team.name}" }
+    it { is_expected.to include "[api] remove member #{diff[:remove_members][team][:members][0]} from team #{team.name}" }
+    it { is_expected.to include "[api] add repo #{diff[:add_repos][team][:repos][0]} to team #{team.name}" }
+    it { is_expected.to include "[api] remove repo #{diff[:remove_repos][team][:repos][0]} from team #{team.name}" }
+  end
+
+  context 'without changes' do
+    subject { described_class.new(empty_diff).now! }
+
+    it { is_expected.to satisfy { |s| s.size == 1 } }
+    it { is_expected.to include "There are no changes." }
+  end
 end
