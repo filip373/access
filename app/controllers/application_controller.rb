f3cf00d09da94a4514977abd85aad26361157549
Mateusz Stebnicki <mateusz.stebnicki@netguru.pl>
2014-12-02 14:50:36 +0100

    add google_auth_required method
    [#80824248]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_filter :such_auth_required
+  before_filter :gh_auth_required
+  before_filter :google_auth_required
 
-  def such_auth_required
+  def gh_auth_required
     redirect_to '/auth/github' unless session[:token].present?
+  end
+
+  def google_auth_required
+    redirect_to '/auth/google_oauth2' unless session[:google_token].present?
   end
 end
