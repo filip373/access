e2555bd05fdef0f99c3cfb5e487377390e1313c4
Mateusz Stebnicki <mateusz.stebnicki@netguru.pl>
2014-12-08 10:59:43 +0100

    update main controller
    [#80824248]
    - now DRY it all up!

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,19 +1,25 @@
 class MainController < ApplicationController
   before_action :check_permissions
+  include GoogleIntegration
   include GithubIntegration
 
   expose(:gh_api) { GithubIntegration::Api.new(session[:token], AppConfig.company) }
+  expose(:google_api) { GoogleIntegration::Api.new(session[:google_token]) }
 
   expose(:expected_teams) { GithubIntegration::Teams.all }
+  expose(:expected_groups) { GoogleIntegration::Groups.all }
 
   expose(:validation_errors) { Storage.validation_errors }
   expose(:update_repo) { UpdateRepo.new }
 
   expose(:gh_diff) { GithubIntegration::Actions::GetDiff.new(expected_teams, gh_api) }
+  expose(:google_diff) { GoogleIntegration::Actions::GetDiff.new(expected_groups, google_api) }
 
   expose(:sync_github) { GithubIntegration::Actions::SyncTeams.new(gh_api) }
+  expose(:sync_google) { GoogleIntegration::Actions::SyncGroups.new(google_api) }
 
   expose(:get_gh_log) { GithubIntegration::Actions::GetLog.new(get_gh_diff) }
+  expose(:get_google_log) { GoogleIntegration::Actions::GetLog.new(get_google_diff) }
 
   expose(:teams_cleanup) { GithubIntegration::Actions::CleanupTeams.new(expected_teams, gh_api) }
   expose(:missing_teams) { teams_cleanup.stranded_teams }
@@ -21,12 +27,17 @@ class MainController < ApplicationController
   def show_diff
     @gh_diff = gh_diff.now!
     @gh_log = get_gh_log.now!
+    @google_diff = google_diff.now!
+    @google_log = get_google_log.now!
+    @google_log = []
     render
   end
 
   def do_sync
     sync_github.now!(get_gh_diff)
+    sync_google.now!(get_google_diff)
     @gh_diff = nil
+    @google_diff = nil
     render
   end
 
@@ -49,5 +60,9 @@ class MainController < ApplicationController
 
   def get_gh_diff
     @gh_diff ||= gh_diff.now!
+  end
+
+  def get_google_diff
+    @google_diff ||= google_diff.now!
   end
 end
