06525aad1821e02a73fbeb49fb86ea0b128f55df
Mateusz Stebnicki <mateusz.stebnicki@netguru.pl>
2014-12-09 19:12:00 +0100

    ref google log
    [#80824248]

--- app/models/google_integration/actions/get_log.rb
+++ app/models/google_integration/actions/get_log.rb
@@ -1,74 +1,28 @@
-module GoogleIntegration
-  module Actions
-    class GetLog
+module Log
+  class Google < Base
 
-      def initialize(diff_hash)
-        @diff_hash = diff_hash
-        @log = []
-      end
+    create_model({ model: :group,
+      models: :groups,
+      mod_prop: :email,
+      items: [:members, :aliases],
+      new_model_items: [:members, :aliases] })
 
-      def now!
-        generate_log
-      end
+    def initialize(diff)
+      super
+    end
 
-      private
+    def now!
+      # TODO: it should be possible to share opts between methods inside the child class,
+      # this way we could write #now! inside the base class and be done with id.
+      generate_log(:groups, [:members, :aliases])
+    end
 
-      def generate_log
-        log_creating_groups
-        log_adding_members
-        log_removing_members
-        log_adding_aliases
-        log_removing_aliases
-        @log << "There are no changes." if @log.size == 0
-        @log
-      end
+    private
 
-      def log_creating_groups
-        @diff_hash[:create_groups].each do |group, h|
-          @log << "[api] create group #{group.email}"
-
-          h[:add_members].each do |m|
-            @log << "[api] add member #{m} to group #{group.email}"
-          end
-
-          h[:add_aliases].each do |r|
-            @log << "[api] add alias #{r} to group #{group.email}"
-          end
-        end
-      end
-
-      def log_adding_members
-        @diff_hash[:add_members].each do |group, members|
-          members.each do |m|
-            @log << "[api] add member #{m} to group #{group.email}"
-          end
-        end
-      end
-
-      def log_removing_members
-        @diff_hash[:remove_members].each do |group, members|
-          members.each do |m|
-            @log << "[api] remove member #{m} from group #{group.email}"
-          end
-        end
-      end
-
-      def log_adding_aliases
-        @diff_hash[:add_aliases].each do |group, aliases|
-          aliases.each do |a|
-            @log << "[api] add alias #{a} to group #{group.email}"
-          end
-        end
-      end
-
-      def log_removing_aliases
-        @diff_hash[:remove_aliases].each do |group, aliases|
-          aliases.each do |a|
-            @log << "[api] remove alias #{a} from group #{group.email}"
-          end
-        end
-      end
-
+    def generate_log(groups, items)
+      super
+      @log << "There are no changes." if @log.size == 0
+      @log
     end
   end
 end
