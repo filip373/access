baa194e2e9a80234858ca51710807f55c2cb949f
Mateusz Stebnicki <mateusz.stebnicki@netguru.pl>
2014-12-10 17:38:21 +0100

    ref google group sync
    [#80824248]

--- app/models/google_integration/actions/sync_groups.rb
+++ app/models/google_integration/actions/sync_groups.rb
@@ -1,68 +1,24 @@
-module GoogleIntegration
-  module Actions
-    class SyncGroups
+module Sync
+  class Google < Base
 
-      def initialize(google_api)
-        @google_api = google_api
-      end
+    sync_items_methods :members, :aliases
+    new_team_items_methods :members, :aliases
 
-      def now!(diff)
-        sync(diff)
-      end
+    private
 
-      private
+    def sync(diff)
+      create_groups(diff[:create_groups])
+      sync_members(diff[:add_members], diff[:remove_members])
+      sync_aliases(diff[:add_aliases], diff[:remove_aliases])
+    end
 
-      def sync(diff)
-        create_groups(diff[:create_groups])
-        sync_members(diff[:add_members], diff[:remove_members])
-        sync_aliases(diff[:add_aliases], diff[:remove_aliases])
-      end
-
-      def sync_members(members_to_add, members_to_remove)
-        members_to_add.each do |group, members|
-          add_members(group, members)
-        end
-
-        members_to_remove.each do |group, members|
-          members.each do |member|
-            @google_api.remove_from_group(group, member)
-          end
+    def create_groups(groups_to_create)
+      groups_to_create.each do |group, h|
+        @google_api.create_group(group.name) do |created_group|
+          new_team_add_members(h[:add_members], created_group)
+          new_team_add_aliases(h[:add_aliases], created_group)
         end
       end
-
-      def sync_aliases(aliases_to_add, aliases_to_remove)
-        aliases_to_add.each do |group, aliases|
-          add_aliases(group, aliases)
-        end
-
-        aliases_to_remove.each do |group, aliases|
-          aliases.each do |google_alias|
-            @google_api.remove_alias(group, google_alias)
-          end
-        end
-      end
-
-      def create_groups(groups_to_create)
-        groups_to_create.each do |group, h|
-          @google_api.create_group(group.name) do |created_group|
-            add_members(h[:add_members], created_group)
-            add_aliases(h[:add_aliases], created_group)
-          end
-        end
-      end
-
-      def add_members(group, members)
-        members.each do |member|
-          @google_api.add_member(group, member)
-        end
-      end
-
-      def add_aliases(group, aliases)
-        aliases.each do |google_alias|
-          @google_api.add_alias(group, google_alias)
-        end
-      end
-
     end
   end
 end
