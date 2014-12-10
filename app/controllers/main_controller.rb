62f80dbca06cdaffc9a71f49d3dd99dd48c1bb15
Mateusz Stebnicki <mateusz.stebnicki@netguru.pl>
2014-12-10 17:26:00 +0100

    update main_controller
    [#80824248]
    google related code is commented out, since I haven’t had the change to
    test it against a real api

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,13 @@
 class MainController < ApplicationController
   before_action :check_permissions
-  include GoogleIntegration
-  include GithubIntegration
+  require Rails.root.join("app/models/actions/get_log")
+  require Rails.root.join("app/models/actions/get_diff")
+  require Rails.root.join("app/models/actions/sync")
+  require Rails.root.join("app/models/github_integration/actions/get_log")
+  require Rails.root.join("app/models/github_integration/actions/get_diff")
+  require Rails.root.join("app/models/github_integration/actions/sync_teams")
+  require Rails.root.join("app/models/google_integration/actions/get_log")
+  require Rails.root.join("app/models/google_integration/actions/get_diff")
 
   expose(:gh_api) { GithubIntegration::Api.new(session[:token], AppConfig.company) }
   expose(:google_api) { GoogleIntegration::Api.new(session[:google_token]) }
@@ -12,14 +18,14 @@ class MainController < ApplicationController
   expose(:validation_errors) { Storage.validation_errors }
   expose(:update_repo) { UpdateRepo.new }
 
-  expose(:gh_diff) { GithubIntegration::Actions::GetDiff.new(expected_teams, gh_api) }
-  expose(:google_diff) { GoogleIntegration::Actions::GetDiff.new(expected_groups, google_api) }
+  expose(:gh_diff) { Diff::Github.new(expected_teams, gh_api) }
+  # expose(:google_diff) { Diff::Google.new(expected_groups, google_api) }
 
-  expose(:sync_github) { GithubIntegration::Actions::SyncTeams.new(gh_api) }
-  expose(:sync_google) { GoogleIntegration::Actions::SyncGroups.new(google_api) }
+  expose(:get_gh_log) { Log::Github.new(get_gh_diff) }
+  # expose(:get_google_log) { Log::Google.new(get_google_diff) }
 
-  expose(:get_gh_log) { GithubIntegration::Actions::GetLog.new(get_gh_diff) }
-  expose(:get_google_log) { GoogleIntegration::Actions::GetLog.new(get_google_diff) }
+  expose(:sync_github) { Sync::Github.new(gh_api) }
+  # expose(:sync_google) { GoogleIntegration::Actions::SyncGroups.new(google_api) }
 
   expose(:teams_cleanup) { GithubIntegration::Actions::CleanupTeams.new(expected_teams, gh_api) }
   expose(:missing_teams) { teams_cleanup.stranded_teams }
@@ -27,16 +33,15 @@ class MainController < ApplicationController
   def show_diff
     @gh_diff = gh_diff.now!
     @gh_log = get_gh_log.now!
-    @google_diff = google_diff.now!
-    @google_log = get_google_log.now!
+    # @google_diff = google_diff.now!
+    # @google_log = get_google_log.now! || []
     @google_log = []
   end
 
   def do_sync
     sync_github.now!(get_gh_diff)
-    sync_google.now!(get_google_diff)
-    @gh_diff = nil
-    @google_diff = nil
+    # sync_google.now!(get_google_diff)
+    reset_diffs
   end
 
   def cleanup_teams
@@ -55,11 +60,16 @@ class MainController < ApplicationController
 
   private
 
+  def reset_diffs
+    @gh_diff = nil
+    @google_diff = nil
+  end
+
   def get_gh_diff
     @gh_diff ||= gh_diff.now!
   end
 
   def get_google_diff
-    @google_diff ||= google_diff.now!
+    # @google_diff ||= google_diff.now!
   end
 end
