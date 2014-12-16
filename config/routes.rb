032f551363f0dd96fb34fa9a2808e7f4dee712b8
Mateusz Stebnicki <mateusz.stebnicki@netguru.pl>
2014-12-16 12:02:56 +0100

    routes for new ctrl
    [#84659922]

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,14 @@ GithubApp::Application.routes.draw do
   get '/auth/:provider/callback' => 'session#create'
   get '/logout' => 'session#destroy', as: 'logout'
 
-  resources :main, only: [:index] do
-    collection do
-      get :show_diff
-      post :do_sync
-      delete :cleanup_teams
-    end
+  namespace :github do
+    get 'show_diff' => 'github_integration#show_diff', as: 'show_diff'
+    post 'sync' => 'github_integration#sync', as: 'sync'
+    delete 'cleanup_teams' => 'github_integration#cleanup_teams', as: 'cleanup_teams'
+  end
+
+  namespace :google do
+    get 'show_diff' => 'google_integration#show_diff', as: 'show_diff'
+    post 'sync' => 'google_integration#sync', as: 'sync'
   end
 end
