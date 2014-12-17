05c61662d68208a762331c16179b47f51815611e
Mateusz Stebnicki <mateusz.stebnicki@netguru.pl>
2014-12-17 11:51:01 +0100

    create separate sessions for each provider
    [#84674200]

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,20 +1,14 @@
 class SessionController < ApplicationController
 
-  skip_before_filter :gh_auth_required, only: [:create, :destroy]
-  skip_before_filter :google_auth_required, only: [:create, :destroy]
-
-  expose(:auth_hash){ request.env['omniauth.auth'].with_indifferent_access }
-
-  def create
-    session[:token] = auth_hash[:credentials][:token] if auth_hash[:provider] == 'github'
-    session[:google_token] = auth_hash[:credentials][:token] if auth_hash[:provider] == 'google_oauth2'
-    redirect_to main_index_path
-  end
+  skip_before_filter :gh_auth_required, only: [:destroy, :failure]
+  skip_before_filter :google_auth_required, only: [:destroy, :failure]
 
   def destroy
-    session[:token] = nil
+    session[:gh_token] = nil
     session[:google_token] = nil
     redirect_to root_path
   end
 
+  def failure
+  end
 end
