874a966632142e05877648683c8d760cd4b4e410
Mateusz Stebnicki <mateusz.stebnicki@netguru.pl>
2014-12-17 12:43:41 +0100

    Revert "ref google group sync"
    This reverts commit 9100a5fe47796a5a8e77ce07e600133e0ff544b5.
    
    Conflicts:
    	app/models/github_integration/actions/sync_teams.rb

--- app/models/google_integration/actions/sync.rb
+++ app/models/google_integration/actions/sync.rb
@@ -1,9 +1,14 @@
 module GoogleIntegration
   module Actions
-    class Sync < BaseActions::Sync::Base
+    class Sync
 
-      sync_items_methods :members, :aliases
-      new_team_items_methods :members, :aliases
+      def initialize(google_api)
+        @google_api = google_api
+      end
+
+      def now!(diff)
+        sync(diff)
+      end
 
       private
 
@@ -13,14 +18,51 @@ module GoogleIntegration
         sync_aliases(diff[:add_aliases], diff[:remove_aliases])
       end
 
-      def create_groups(groups_to_create)
-        groups_to_create.each do |group, h|
-          @google_api.create_group(group.name) do |created_group|
-            new_team_add_members(h[:add_members], created_group)
-            new_team_add_aliases(h[:add_aliases], created_group)
+      def sync_members(members_to_add, members_to_remove)
+        members_to_add.each do |group, members|
+          add_members(group, members)
+        end
+
+        members_to_remove.each do |group, members|
+          members.each do |member|
+            @google_api.remove_from_group(group, member)
           end
         end
       end
+
+      def sync_aliases(aliases_to_add, aliases_to_remove)
+        aliases_to_add.each do |group, aliases|
+          add_aliases(group, aliases)
+        end
+
+        aliases_to_remove.each do |group, aliases|
+          aliases.each do |google_alias|
+            @google_api.remove_alias(group, google_alias)
+          end
+        end
+      end
+
+      def create_groups(groups_to_create)
+        groups_to_create.each do |group, h|
+          @google_api.create_group(group.name) do |created_group|
+            add_members(h[:add_members], created_group)
+            add_aliases(h[:add_aliases], created_group)
+          end
+        end
+      end
+
+      def add_members(group, members)
+        members.each do |member|
+          @google_api.add_member(group, member)
+        end
+      end
+
+      def add_aliases(group, aliases)
+        aliases.each do |google_alias|
+          @google_api.add_alias(group, google_alias)
+        end
+      end
+
     end
   end
 end
