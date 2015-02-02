706fe0ad0e237f9611ef1fce9a6450de87e70321
Marcin Stecki <marcin@netguru.pl>
2015-02-02 18:13:01 +0100

    :ok: rubocop - omniauth config, add hd param

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,13 +1,19 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-	provider :developer unless Rails.env.production?
-	provider :github, AppConfig.github.client_id, AppConfig.github.client_secret, scope: 'admin:org,repo'
-	provider :google_oauth2, AppConfig.google.client_id, AppConfig.google.client_secret,
-		{
-			scope: AppConfig.google.scope,
-			prompt: "select_account",
-			image_aspect_ratio: "square",
-			image_size: 50,
-			access_type: 'offline',
-			approval_prompt: 'force'
-		}
+  provider :developer unless Rails.env.production?
+
+  provider :github,
+           AppConfig.github.client_id,
+           AppConfig.github.client_secret,
+           scope: 'admin:org,repo'
+
+  provider :google_oauth2,
+           AppConfig.google.client_id,
+           AppConfig.google.client_secret,
+           scope: AppConfig.google.scope,
+           prompt: 'select_account',
+           image_aspect_ratio: 'square',
+           image_size: 50,
+           access_type: 'offline',
+           approval_prompt: 'force',
+           hd: AppConfig.google.main_domain
 end
