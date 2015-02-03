7da11109eec7cb091e5c6e695664ff23d2091dfb
Marcin Stecki <marcin@netguru.pl>
2015-02-03 14:47:58 +0100

    :ok: fix login issues in gapps controller
    [#87294348]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,13 +4,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_filter :gh_auth_required
-  before_filter :google_auth_required, if: -> { AppConfig.features.google_integration }
 
   def gh_auth_required
     redirect_to '/auth/github' unless session[:gh_token].present?
-  end
-
-  def google_auth_required
-    redirect_to '/auth/google_oauth2' unless session[:google_token].present?
   end
 end
--- app/controllers/google_integration/main_controller.rb
+++ app/controllers/google_integration/main_controller.rb
@@ -1,11 +1,13 @@
 module GoogleIntegration
   class MainController < ApplicationController
-
     expose(:google_api) { GoogleIntegration::Api.new(session[:google_token]) }
     expose(:expected_groups) { GoogleIntegration::Groups.all }
     expose(:google_diff) { GoogleIntegration::Actions::Diff.new(expected_groups, google_api) }
     expose(:get_google_log) { GoogleIntegration::Actions::Log.new(get_google_diff) }
     expose(:sync_google_job) { GoogleIntegration::SyncJob.new }
+
+    before_filter :google_auth_required, unless: :google_logged_in?
+    rescue_from OAuth2::Error, with: :google_auth_required
 
     def show_diff
       @google_diff = google_diff.now!
@@ -25,6 +27,14 @@ module GoogleIntegration
 
     def get_google_diff
       @google_diff ||= google_diff.now!
+    end
+
+    def google_auth_required
+      redirect_to '/auth/google_oauth2'
+    end
+
+    def google_logged_in?
+      session[:google_token].present?
     end
   end
 end
