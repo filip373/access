1e75675d9edac46824ff7f70bfb742ff7491c7be
Marcin Stecki <marcin@netguru.pl>
2015-02-05 16:07:51 +0100

    :ok: fix notification
    [#87294348]

--- app/models/google_integration/sync_job.rb
+++ app/models/google_integration/sync_job.rb
@@ -9,17 +9,17 @@ module GoogleIntegration
     end
 
     def before
-      msg = "Synchronizing google groups..."
+      msg = 'Synchronizing google groups...'
       notification_ping! msg
     end
 
     def after
-      msg = "Synchronization done! High Five!"
+      msg = 'Synchronization done! High Five!'
       notification_ping! msg
     end
 
-    def notification_ping! msg
-      notifier = SlackNotification.new(ENV['SLACK_TOKEN'], ENV['SLACK_CHANNEL'], ENV['REVIEW_PATH'], ENV['APP_URL'], message: msg)
+    def notification_ping!(_msg)
+      notifier = SlackNotification.new(message: msg)
       notifier.ping!
     end
   end
