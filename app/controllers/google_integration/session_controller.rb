22a97890b95321a7b14c015f48f1032f823947ff
Marcin Stecki <marcin@netguru.pl>
2015-02-05 16:10:53 +0100

    :ok: redirect to diff after login
    [#87294348]

--- app/controllers/google_integration/session_controller.rb
+++ app/controllers/google_integration/session_controller.rb
@@ -8,7 +8,7 @@ module GoogleIntegration
 
     def create
       session[:google_token] = auth_hash[:credentials][:token] if auth_hash[:provider] == 'google_oauth2'
-      redirect_to root_path
+      redirect_to google_show_diff_path
     end
   end
 end
