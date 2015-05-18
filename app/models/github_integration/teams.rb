e9a92b9a9212fc54383129d1fce4ecd99150db7a
Mariusz Błaszczak <mariusz.blaszczak@netguru.co>
2015-05-18 15:50:50 +0200

    Not raise exception if there is no repos [#85992450]

--- app/models/github_integration/teams.rb
+++ app/models/github_integration/teams.rb
@@ -1,13 +1,12 @@
 module GithubIntegration
   class Teams
-
     def self.all
       raw_data.map do |team_name, team_data|
         Team.new(
           team_name,
           team_data.members,
-          team_data.repos,
-          team_data.permission || default_permission
+          team_data.repos || [],
+          team_data.permission || default_permission,
         )
       end
     end
@@ -29,5 +28,4 @@ module GithubIntegration
 
   class Team < Struct.new(:name, :members, :repos, :permission)
   end
-
 end
