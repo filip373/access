6df033ee0771f1a6f6deef837aaea8b36cdfa40b
Mariusz Błaszczak <mariusz.blaszczak@netguru.co>
2015-05-25 07:44:58 +0200

    Clear cache after all specs [#95028062]

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,6 +3,7 @@ ENV['RAILS_ENV'] ||= 'test'
 require 'spec_helper'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
+require 'celluloid/test'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -30,4 +31,7 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
+  config.after(:all) do
+    Rails.cache.clear
+  end
 end
