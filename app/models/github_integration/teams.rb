bd4e1722598e88a0b126bc687d5eee0695867ee2
Mariusz Błaszczak <mariusz.blaszczak@netguru.co>
2015-05-26 12:33:40 +0200

    hotfix [#95341556]

--- app/models/github_integration/teams.rb
+++ app/models/github_integration/teams.rb
@@ -5,8 +5,8 @@ module GithubIntegration
       raw_data.map do |team_name, team_data|
         Team.new(
           team_name,
-          team_data.members,
-          team_data.repos,
+          team_data.members || [],
+          team_data.repos || [],
           team_data.permission || default_permission
         )
       end
