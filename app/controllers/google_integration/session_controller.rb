4f4214fe3aabb1c7d9578c231280be6fda7bb0d0
Mariusz Błaszczak <mariusz.blaszczak@netguru.co>
2015-06-11 17:37:57 +0200

    Refactor create session to use new gem [#95617152]

--- app/controllers/google_integration/session_controller.rb
+++ app/controllers/google_integration/session_controller.rb
@@ -8,7 +8,8 @@ module GoogleIntegration
     end
 
     def create
-      session[:google_token] = auth_hash[:credentials][:token] if auth_hash[:provider] == 'google_oauth2'
+      authorize_auth_client!
+      session[:credentials] = auth_client_json
       redirect_to google_show_diff_path
     end
 
@@ -16,6 +17,32 @@ module GoogleIntegration
 
     def authorization_uri
       auth_client.authorization_uri.to_s
+    end
+
+    def auth_client
+      return @auth_client if @auth_client.present?
+      @auth_client = client_secrets.to_authorization
+      @auth_client.update!(
+        scope: AppConfig.google.scope,
+      )
+      @auth_client
+    end
+
+    def client_secrets
+      client_secret_json = Rails.root.join(AppConfig.google.config_json)
+      ::Google::APIClient::ClientSecrets.load(client_secret_json)
+    end
+
+    def authorize_auth_client!
+      auth_client.code = request['code']
+      auth_client.fetch_access_token!
+    end
+
+    def auth_client_json
+      auth_client_json = JSON.parse(auth_client.to_json)
+      auth_client_json['redirect_uri'] = auth_client.redirect_uri.to_s
+      auth_client_json['client_secret'] = nil
+      auth_client_json.compact.to_json
     end
   end
 end
