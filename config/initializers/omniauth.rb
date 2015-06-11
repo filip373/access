01395ef36a3666eb9371ee0116e5777fbb58a738
Mariusz Błaszczak <mariusz.blaszczak@netguru.co>
2015-06-11 17:35:38 +0200

    Not required anymore [#95617152]

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -5,15 +5,4 @@ Rails.application.config.middleware.use OmniAuth::Builder do
            AppConfig.github.client_id,
            AppConfig.github.client_secret,
            scope: 'admin:org,repo'
-
-  provider :google_oauth2,
-           AppConfig.google.client_id,
-           AppConfig.google.client_secret,
-           scope: AppConfig.google.scope,
-           prompt: 'select_account',
-           image_aspect_ratio: 'square',
-           image_size: 50,
-           access_type: 'offline',
-           approval_prompt: 'force',
-           hd: AppConfig.google.main_domain
 end
