3a22e621db64b82ef18b7258d8967ebdd5b58fb8
Mariusz Błaszczak <mariusz.blaszczak@netguru.co>
2015-06-11 17:37:33 +0200

    Add new method to session controller which goes to google auth [#95617152]

--- app/controllers/google_integration/session_controller.rb
+++ app/controllers/google_integration/session_controller.rb
@@ -3,10 +3,19 @@ module GoogleIntegration
     skip_before_filter :gh_auth_required, only: [:create]
     skip_before_filter :google_auth_required, only: [:create]
 
+    def new
+      redirect_to authorization_uri
+    end
 
     def create
       session[:google_token] = auth_hash[:credentials][:token] if auth_hash[:provider] == 'google_oauth2'
       redirect_to google_show_diff_path
+    end
+
+    private
+
+    def authorization_uri
+      auth_client.authorization_uri.to_s
     end
   end
 end
