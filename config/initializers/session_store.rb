58e96098a2e511bdd6f013d6c503228f7af4d398
Piotr Jurczynski <piotr.jurczynski@netguru.co>
2015-06-15 10:27:40 +0200

    use redis as a session store
    [#94946956]

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
-GithubApp::Application.config.session_store :cookie_store, key: '_github_app_session'
+GithubApp::Application.config.session_store :redis_store,
+  servers: 'redis://localhost:6379/0/cache'
