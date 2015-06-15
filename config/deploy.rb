61d59493457bda376b4af49330c547f74d393cf1
Piotr Jurczynski <piotr.jurczynski@netguru.co>
2015-06-15 12:22:05 +0200

    restart passenger with 'restart.txt'
    [#94946956]

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,3 +3,4 @@ set :repo_url, 'git://github.com/netguru/access.git'
 set :deploy_to, ENV["DEPLOY_PATH"]
 set :linked_files, %w{config/sec_config.yml}
 set :linked_dirs, %w{bin log tmp vendor/bundle }
+set :passenger_restart_with_touch, true
