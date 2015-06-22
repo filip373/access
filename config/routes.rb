9d7a810cdc9872e08276ce33a5c064e5c076a649
Piotr Jurczynski <piotr.jurczynski@netguru.co>
2015-06-22 19:08:20 +0200

    show button to generate permissions
    [#96531382]

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,12 @@ GithubApp::Application.routes.draw do
   get 'github/show_diff' => 'github_integration/main#show_diff', as: 'github_show_diff'
   post 'github/sync' => 'github_integration/main#sync', as: 'github_sync'
   delete 'github/cleanup_teams' => 'github_integration/main#cleanup_teams', as: 'github_cleanup_teams'
+  get 'github/generate_permissions' => 'github_integration/generate#permissions'
 
-  get 'google/generate_permissions' => 'google_integration/generate#permissions'
   get 'google/show_diff' => 'google_integration/main#show_diff', as: 'google_show_diff'
   post 'google/sync' => 'google_integration/main#sync', as: 'google_sync'
   post 'google/create_accounts' => 'google_integration/main#create_accounts', as: 'create_accounts'
   delete 'google/cleanup_groups' => 'google_integration/main#cleanup_groups', as: 'google_cleanup_groups'
+  get 'google/generate_permissions' => 'google_integration/generate#permissions'
 
 end
