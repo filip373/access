3b2112d71c336b61822bdbf44e599b72e05618b7
Piotr Jurczynski <piotr.jurczynski@netguru.co>
2015-06-22 19:05:03 +0200

    build team from api request
    [#96531382]

--- app/models/github_integration/team.rb
+++ app/models/github_integration/team.rb
@@ -1,5 +1,14 @@
 module GithubIntegration
   class Team
     rattr_initialize :name, :members, :repos, :permission
+
+    def self.from_api_request(client, team)
+      new(
+        team.name,
+        client.list_team_members(team.id).map(&:login),
+        client.list_team_repos(team.id).map(&:name).uniq,
+        team.permission,
+      )
+    end
   end
 end
