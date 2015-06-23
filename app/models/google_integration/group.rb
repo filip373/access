8db29e590c9ed3060a1032eb7a17aafe48d876a1
Piotr Jurczynski <piotr.jurczynski@netguru.co>
2015-06-23 09:24:44 +0200

    group uses group privacy model
    [#96531382]

--- app/models/google_integration/group.rb
+++ app/models/google_integration/group.rb
@@ -1,6 +1,10 @@
 module GoogleIntegration
   class Group
-    rattr_initialize :name, :members, :aliases, :domain_membership, :privacy, :archive
+    rattr_initialize :name, :members, :aliases, :domain_membership, :privacy, :archive do
+      @privacy = GoogleIntegration::GroupPrivacy.from_string(@privacy)
+    end
+
+    delegate :open?, :closed?, :who_can_view_group, :show_in_group_directory?, to: :privacy
 
     def email
       "#{name}@#{AppConfig.google.main_domain}"
@@ -22,29 +26,9 @@ module GoogleIntegration
       @users ||= build_users
     end
 
-    def show_in_group_directory?
-      open?
-    end
-
-    def who_can_view_group
-      if closed?
-        'ALL_MEMBERS_CAN_VIEW'
-      else
-        'ALL_IN_DOMAIN_CAN_VIEW'
-      end
-    end
-
     def archive?
       return archive unless archive.nil?
       false
-    end
-
-    def closed?
-      privacy == 'closed'
-    end
-
-    def open?
-      privacy.nil? || privacy == 'open'
     end
 
     def to_yaml
