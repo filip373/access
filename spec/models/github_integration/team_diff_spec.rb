836d638261bb015bc4115a07d04d5c9cf24a0acb
Piotr Jurczynski <piotr.jurczynski@netguru.co>
2015-06-23 09:27:06 +0200

    remove binding.pry
    [#96531382]

--- spec/models/github_integration/team_diff_spec.rb
+++ spec/models/github_integration/team_diff_spec.rb
@@ -62,7 +62,6 @@ RSpec.describe GithubIntegration::TeamDiff do
         team_diff.async.diff(blk)
         errors = condition.wait
         expect(errors).to_not be_empty
-        binding.pry if errors.count == 4
         expect(errors.count).to eq(1)
       end
     end
