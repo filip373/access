472a6d915e258d704f65daaba66937ca0dcd8952
Piotr Jurczynski <piotr.jurczynski@netguru.co>
2015-06-23 09:25:14 +0200

    create a group based on google api response
    [#96531382]

--- app/models/google_integration/group.rb
+++ app/models/google_integration/group.rb
@@ -6,6 +6,18 @@ module GoogleIntegration
 
     delegate :open?, :closed?, :who_can_view_group, :show_in_group_directory?, to: :privacy
 
+    def self.from_google_api(group)
+      privacy = GoogleIntegration::GroupPrivacy.from_google_api(group)
+      new(
+        username(group.email),
+        group.members,
+        group.aliases,
+        !!group.members.find { |member| member['id'] == AppConfig.google.domain_member_id },
+        privacy.to_s,
+        group.settings.isArchived == 'true',
+      )
+    end
+
     def email
       "#{name}@#{AppConfig.google.main_domain}"
     end
@@ -34,19 +46,19 @@ module GoogleIntegration
     def to_yaml
       {
         domain_membership: domain_membership,
-        members: Array(members).map { |user| username(user.email || '') }.compact,
-        aliases: Array(aliases).map { |email| username(email) }.compact,
-        privacy: privacy,
+        members: Array(members).map { |user| Group.username(user.email || '') }.compact,
+        aliases: Array(aliases).map { |email| Group.username(email) }.compact,
+        privacy: privacy.to_s,
         archive: archive,
       }.stringify_keys.to_yaml
     end
 
-    def username(email)
-      email.split('@').first
-    end
-
     def errors
       nil
+    end
+
+    def self.username(email)
+      email.split('@').first
     end
   end
 end
