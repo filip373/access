68122ed6fe2cdafecd5601b8496cee0e99378d9d
Piotr Jurczynski <piotr.jurczynski@netguru.co>
2015-07-03 16:01:37 +0200

    get info about a user that was authenticated
    [#97176964]

--- app/models/google_integration/api/user_account_authorization.rb
+++ app/models/google_integration/api/user_account_authorization.rb
@@ -4,10 +4,26 @@ class GoogleIntegration::Api
       client
     end
 
+    def email
+      user_info.email
+    end
+
+    def user_info
+      return @user_info if @user_info.present?
+      response = client.fetch_protected_resource(
+        uri: user_info_uri(client.access_token)
+      )
+      @user_info = Hashie::Mash.new(JSON.parse(response.body))
+    end
+
     private
 
     def client
       @client ||= Signet::OAuth2::Client.new(credentials)
+    end
+
+    def user_info_uri(access_token)
+      "https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=#{access_token}"
     end
   end
 end
