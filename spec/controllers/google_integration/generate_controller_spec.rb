8e398f1e5c992b4cb30a4222d5da5ea02a85e921
Piotr Jurczynski <piotr.jurczynski@netguru.co>
2015-07-06 11:01:18 +0200

    create a context for permitted google groups

--- spec/controllers/google_integration/generate_controller_spec.rb
+++ spec/controllers/google_integration/generate_controller_spec.rb
@@ -9,47 +9,52 @@ module GoogleIntegration
     before(:each) do
       allow(controller).to receive(:google_auth_required) { true }
       allow(controller).to receive(:gh_auth_required) { true }
-      if AppConfig.features.use_service_account?
-        allow(controller).to receive(:unauthorized_access).and_return(true)
-      end
       allow(google_api).to receive(:list_groups_full_info) { groups }
       allow(GoogleIntegration::Api).to receive(:new).and_return(google_api)
     end
 
-    describe '#permissions' do
-      let(:permissions_dir) { Rails.root.join('tmp/test_permissions') }
-
-      describe 'creating google groups' do
-        before do
-          google_api = double
-          allow(google_api).to receive(:list_groups_full_info).and_return(groups)
-          allow(controller).to receive(:google_api).and_return(google_api)
-          allow(controller).to receive(:permissions_dir).and_return(permissions_dir)
-          get :permissions
+    context 'is permitted to manage google groups' do
+      before do
+        if AppConfig.features.use_service_account?
+          allow(controller).to receive(:google_authorized?).and_return(true)
         end
+      end
 
-        after { FileUtils.rm_rf(permissions_dir) }
+      describe '#permissions' do
+        let(:permissions_dir) { Rails.root.join('tmp/test_permissions') }
 
-        let(:groups) do
-          JSON.load(Rails.root.join('spec/fixtures/google/team_example.json')).map do |group|
-            Hashie::Mash.new group
+        describe 'creating google groups' do
+          before do
+            google_api = double
+            allow(google_api).to receive(:list_groups_full_info).and_return(groups)
+            allow(controller).to receive(:google_api).and_return(google_api)
+            allow(controller).to receive(:permissions_dir).and_return(permissions_dir)
+            get :permissions
           end
-        end
 
-        subject { YAML.load_file(permissions_dir.join('google_groups/team.yml')) }
+          after { FileUtils.rm_rf(permissions_dir) }
 
-        it 'creates a google group' do
-          expect(File.exist?(permissions_dir.join('google_groups/team.yml'))).to be_truthy
-          expect(subject).not_to be_falsy
-        end
+          let(:groups) do
+            JSON.load(Rails.root.join('spec/fixtures/google/team_example.json')).map do |group|
+              Hashie::Mash.new group
+            end
+          end
 
-        describe 'contents of yaml file' do
-          let(:group) { groups.first }
-          it { expect(subject['members']).to match ['jon.hanks', 'beth.hamilthon'] }
-          it { expect(subject['aliases']).to eq [] }
-          it { expect(subject['domain_membership']).to eq false }
-          it { expect(subject['privacy']).to eq 'open' }
-          it { expect(subject['archive']).to eq true }
+          subject { YAML.load_file(permissions_dir.join('google_groups/team.yml')) }
+
+          it 'creates a google group' do
+            expect(File.exist?(permissions_dir.join('google_groups/team.yml'))).to be_truthy
+            expect(subject).not_to be_falsy
+          end
+
+          describe 'contents of yaml file' do
+            let(:group) { groups.first }
+            it { expect(subject['members']).to match ['jon.hanks', 'beth.hamilthon'] }
+            it { expect(subject['aliases']).to eq [] }
+            it { expect(subject['domain_membership']).to eq false }
+            it { expect(subject['privacy']).to eq 'open' }
+            it { expect(subject['archive']).to eq true }
+          end
         end
       end
     end
--- spec/controllers/google_integration/main_controller_spec.rb
+++ spec/controllers/google_integration/main_controller_spec.rb
@@ -7,57 +7,62 @@ RSpec.describe GoogleIntegration::MainController do
   before(:each) do
     allow(controller).to receive(:google_auth_required).and_return(true)
     allow(controller).to receive(:gh_auth_required).and_return(true)
-    if AppConfig.features.use_service_account?
-      allow(controller).to receive(:unauthorized_access).and_return(true)
-    end
     allow(GoogleIntegration::Api).to receive(:new).and_return(google_api)
     UpdateRepo.stub(:now!).and_return(true)
   end
 
-  describe 'GET show_diff' do
-    before { get :show_diff }
-
-    it { expect(controller.google_log).to be_a Array }
-    it { expect(controller.expected_groups).to be_a Array }
-    it { expect(response).to render_template('show_diff') }
-
-    it { expect(controller.missing_accounts).to be_a Array }
-    it { expect(controller.missing_accounts).to_not be_empty }
-
-    it 'caches missing accounts' do
-      controller.missing_accounts # Needs to use exposed variable since views are not rendered
-      expect(Rails.cache.read('calculated_missing_accounts')).to_not be_nil
-      expect(Rails.cache.read('calculated_missing_accounts')).to be_a Array
-      expect(Rails.cache.read('calculated_missing_accounts')).to_not be_empty
-    end
-
-    it 'caches google_diff value' do
-      controller.google_log # Needs to use exposed variable since views are not rendered
-      expect(Rails.cache.read('calculated_diff')).to_not be_nil
-      expect(Rails.cache.read('calculated_diff')).to be_a Hash
-      expect(Rails.cache.read('calculated_diff')).to_not be_empty
-    end
-  end
-
-  describe 'POST create_accounts' do
+  context 'is permitted to manage google groups' do
     before do
-      allow_any_instance_of(GoogleIntegration::Actions::CreateAccounts).to receive(:now!) do
-        {
-          'second.member' => { email: 'second.member@netguru.pl', codes: %w(a b c) },
-          'third.member' => { email: 'third.member@netguru.pl', codes: %w(a b c) },
-        }
+      if AppConfig.features.use_service_account?
+        allow(controller).to receive(:google_authorized?).and_return(true)
       end
-      Rails.cache.fetch('calculated_missing_accounts') { 'calculated_missing_accounts' }
-      ActionMailer::Base.deliveries = []
-      post :create_accounts
     end
 
-    it 'sends emails to office' do
-      expect(ActionMailer::Base.deliveries.count).to eq(2)
+    describe 'GET show_diff' do
+      before { get :show_diff }
+
+      it { expect(controller.google_log).to be_a Array }
+      it { expect(controller.expected_groups).to be_a Array }
+      it { expect(response).to render_template('show_diff') }
+
+      it { expect(controller.missing_accounts).to be_a Array }
+      it { expect(controller.missing_accounts).to_not be_empty }
+
+      it 'caches missing accounts' do
+        controller.missing_accounts # Needs to use exposed variable since views are not rendered
+        expect(Rails.cache.read('calculated_missing_accounts')).to_not be_nil
+        expect(Rails.cache.read('calculated_missing_accounts')).to be_a Array
+        expect(Rails.cache.read('calculated_missing_accounts')).to_not be_empty
+      end
+
+      it 'caches google_diff value' do
+        controller.google_log # Needs to use exposed variable since views are not rendered
+        expect(Rails.cache.read('calculated_diff')).to_not be_nil
+        expect(Rails.cache.read('calculated_diff')).to be_a Hash
+        expect(Rails.cache.read('calculated_diff')).to_not be_empty
+      end
     end
 
-    it 'resets cache' do
-      expect(Rails.cache.read('calculated_missing_accounts')).to be_nil
+    describe 'POST create_accounts' do
+      before do
+        allow_any_instance_of(GoogleIntegration::Actions::CreateAccounts).to receive(:now!) do
+          {
+            'second.member' => { email: 'second.member@netguru.pl', codes: %w(a b c) },
+            'third.member' => { email: 'third.member@netguru.pl', codes: %w(a b c) },
+          }
+        end
+        Rails.cache.fetch('calculated_missing_accounts') { 'calculated_missing_accounts' }
+        ActionMailer::Base.deliveries = []
+        post :create_accounts
+      end
+
+      it 'sends emails to office' do
+        expect(ActionMailer::Base.deliveries.count).to eq(2)
+      end
+
+      it 'resets cache' do
+        expect(Rails.cache.read('calculated_missing_accounts')).to be_nil
+      end
     end
   end
 end
