c30fce9053e862a741ed27a2322ddf09bb1c74df
Adam Nowak <adam.nowak@netguru.co>
2015-07-20 16:07:03 +0200

    remove suspicious line

--- app/models/google_integration/group_policy.rb
+++ app/models/google_integration/group_policy.rb
@@ -1,7 +1,5 @@
 module GoogleIntegration
   class GroupPolicy
-    @groups = Groups.all
-
     def self.edit?(group_email, is_admin = false)
       return true if is_admin
       whitelist.any? { |regexp| regexp.match group_email }
