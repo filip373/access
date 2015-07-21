b36dd114cc3c4b45b7e1b99789fd1425e5807d5a
Marcin Stecki <marcin@netguru.pl>
2015-07-21 10:58:20 +0200

    :ok: dont link bin

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,5 +4,5 @@ set :application, 'access'
 set :repo_url, 'git://github.com/netguru/access.git'
 set :deploy_to, ENV["DEPLOY_PATH"]
 set :linked_files, %w{config/sec_config.yml}
-set :linked_dirs, %w{bin log tmp vendor/bundle config/keys}
+set :linked_dirs, %w{log tmp vendor/bundle config/keys}
 set :passenger_restart_with_touch, true
