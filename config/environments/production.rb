9e20f5d92ece0e24954077b9d0111a6f8ec3983b
Marcin Stecki <marcin@netguru.pl>
2015-07-21 18:40:21 +0200

    :ok: add smtp settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,4 +11,20 @@ GithubApp::Application.configure do
   config.active_support.deprecation = :notify
 
   config.log_formatter = ::Logger::Formatter.new
+
+
+
+  if AppConfig.smtp?
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      :address => AppConfig.smtp.address,
+      :port => 587,
+      :domain => AppConfig.smtp.domain,
+      :user_name => AppConfig.smtp.user_name,
+      :password => AppConfig.smtp.password,
+      :authentication => "plain"
+    }
+  end
+
+
 end
