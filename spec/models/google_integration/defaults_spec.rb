803d46e541e44a40c87a02e3d8c187609c0dd5f4
Adam Nowak <adam.nowak@netguru.co>
2015-07-23 11:27:26 +0200

    fix spec

--- spec/models/google_integration/defaults_spec.rb
+++ spec/models/google_integration/defaults_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 module GoogleIntegration
   describe Defaults do
     it { expect(described_class.group.privacy).to eq 'open' }
-    it { expect(described_class.group.archive).to eq false }
+    it { expect(described_class.group.archive).to eq true }
     it { expect(described_class.not_existing).to eq nil }
     it { expect(described_class.not_existing { 'passed_block' }).to eq 'passed_block' }
   end
