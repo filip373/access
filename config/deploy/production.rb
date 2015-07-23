d553a250a412fb56a8f7c229bb5e442625a7e508
Adam Nowak <adam.nowak@netguru.co>
2015-07-23 15:23:42 +0200

    add gateway to deploy conf

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,4 @@
 set :branch, "production"
 server ENV["ACCESS_SERVER_HOST"], user: ENV["ACCESS_SERVER_USER"], roles: %w(web app db)
 server ENV["ACCESS_SERVER_HOST_2"], user: ENV["ACCESS_SERVER_USER"], roles: %w(web app)
+set :ssh_options, proxy: Net::SSH::Proxy::Command.new("ssh #{ENV['GATEWAY']} -W %h:%p")
