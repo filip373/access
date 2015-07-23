36d4e7cef5723b1eec28e657be9212a09aa1c1e1
Adam Nowak <adam.nowak@netguru.co>
2015-07-23 15:43:31 +0200

    missing require

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,5 @@
+require 'net/ssh/proxy/command'
+
 set :branch, "production"
 server ENV["ACCESS_SERVER_HOST"], user: ENV["ACCESS_SERVER_USER"], roles: %w(web app db)
 server ENV["ACCESS_SERVER_HOST_2"], user: ENV["ACCESS_SERVER_USER"], roles: %w(web app)
