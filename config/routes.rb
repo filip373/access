f5ba35317f8f2c4b58fa13f7531d45c92328d227
Adam Nowak <adam.nowak@netguru.co>
2015-09-04 20:14:29 +0200

    rewrite routes file to be readable
    no routes were changes expect one

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,40 @@
 GithubApp::Application.routes.draw do
   root 'main#index'
-  get '/auth/github/callback' => 'github_integration/session#create'
-  get '/auth/google_oauth2' => 'google_integration/session#new', as: 'google_oauth2'
-  get '/auth/google_oauth2/callback' => 'google_integration/session#create', as: 'google_oauth2_callback'
 
-  get 'github/show_diff' => 'github_integration/main#show_diff', as: 'github_show_diff'
-  post 'github/sync' => 'github_integration/main#sync', as: 'github_sync'
-  delete 'github/cleanup_teams' => 'github_integration/main#cleanup_teams', as: 'github_cleanup_teams'
-  get 'github/generate_permissions' => 'github_integration/generate#permissions'
+  # AUTH
 
-  get 'google/generate_permissions' => 'google_integration/generate#permissions'
-  get 'generate_users' => 'generate#users'
-  get 'google/show_diff' => 'google_integration/main#show_diff', as: 'google_show_diff'
-  get 'google/show_groups' => 'google_integration/main#show_groups', as: 'google_show_groups'
-  post 'google/sync' => 'google_integration/main#sync', as: 'google_sync'
-  post 'google/create_accounts' => 'google_integration/main#create_accounts', as: 'create_accounts'
-  delete 'google/cleanup_groups' => 'google_integration/main#cleanup_groups', as: 'google_cleanup_groups'
+  scope :auth do
+    namespace :google_oauth2, module: :google_integration  do
+      get '',  to: 'session#new', as: ''
+      get :callback, to: 'session#create'
+    end
+
+    namespace :github, module: :github_integration  do
+      get :callback, to: 'session#create'
+    end
+  end
+
+  namespace :github, module: :github_integration do
+    controller :main do
+      get :show_diff
+      post :sync
+      delete :cleanup_teams
+    end
+    get 'generate_permissions', to: 'generate#permissions'
+  end
+
+  ## INTEGRATIONS
+
+  namespace :google, module: :google_integration do
+    controller :main do
+      get :show_diff
+      get :show_groups
+      post :sync
+      post :create_accounts
+      delete :cleanup_groups
+    end
+    get 'generate_permissions', to: 'generate#permissions'
+  end
+
+  get 'generate_users', to: 'generate#users'
 end
