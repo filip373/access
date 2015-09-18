c29c4959f0712bd760a41783e24d83f17c32b0c2
Mariusz Błaszczak <mariusz.blaszczak@netguru.co>
2015-09-18 10:40:35 +0200

    Add rollbar api RSpec shared_context

---
+++ spec/support/rollbar_api.rb
@@ -0,0 +1,59 @@
+RSpec.shared_context 'rollbar_api' do
+  let(:rollbar_api) do
+    double.tap do |api|
+      allow(api).to receive(:list_teams) { existing_teams }
+      allow(api).to receive(:list_team_members) { existing_members }
+      allow(api).to receive(:list_team_projects) { existing_projects }
+    end
+  end
+
+  let(:team1) do
+    Hashie::Mash.new(
+      name: 'team1',
+      id: 1,
+      account_id: 1,
+    )
+  end
+
+  let(:team2) do
+    Hashie::Mash.new(
+      name: 'team2',
+      id: 2,
+      account_id: 1,
+    )
+  end
+
+  let(:member1) do
+    Hashie::Mash.new(
+      id: 1,
+      username: 'member1',
+      email: 'member1@foo.pl',
+    )
+  end
+
+  let(:member2) do
+    Hashie::Mash.new(
+      id: 2,
+      username: 'member2',
+      email: 'member2@foo.pl',
+    )
+  end
+
+  let(:project1) do
+    Hashie::Mash.new(
+      id: 1,
+      name: 'project1',
+    )
+  end
+
+  let(:project2) do
+    Hashie::Mash.new(
+      id: 2,
+      name: 'project2',
+    )
+  end
+
+  let(:existing_teams) { [team1, team2] }
+  let(:existing_members) { [member1, member2] }
+  let(:existing_projects) { [project1, project2] }
+end
