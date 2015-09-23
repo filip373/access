422855a9e3bc8c107913bde3516f3479a1f03a5c
Mariusz Błaszczak <mariusz.blaszczak@netguru.co>
2015-09-23 08:44:03 +0200

    Add rollbar Teams class

---
+++ app/models/rollbar_integration/teams.rb
@@ -0,0 +1,21 @@
+module RollbarIntegration
+  class Teams
+    def self.all
+      raw_data.map do |team_name, team_data|
+        Team.new(
+          team_name,
+          team_data.members || [],
+          team_data.projects || [],
+        )
+      end
+    end
+
+    def self.raw_data
+      Storage.data.rollbar_teams || []
+    end
+
+    def self.teams_repo_path
+      "#{Rails.root}/tmp/permissions/rollbar_teams"
+    end
+  end
+end
