8c0acf26ac621cd04c174c94e9fbd2c47fca7bda
Mariusz Błaszczak <mariusz.blaszczak@netguru.co>
2015-09-24 15:00:08 +0200

    Refactor structuring diff_hash.
    Now it contains Hashes instead of Arrays in add/remove projects/members

--- app/models/rollbar_integration/team_diff.rb
+++ app/models/rollbar_integration/team_diff.rb
@@ -25,33 +25,38 @@ module RollbarIntegration
 
     private
 
-        server_members = server_team.respond_to?(:fake) ? [] : list_team_members(server_team['id'])
-        @diff_hash[:add_members][server_team] = yaml_members - server_members
-        @diff_hash[:remove_members][server_team] = server_members - yaml_members
     def members_diff
       if server_team.id.present?
+        diff_hash[:add_members][server_team] = add_members
+        diff_hash[:remove_members][server_team] = remove_members
       elsif !yaml_members.empty?
         @diff_hash[:create_teams][server_team][:add_members] = yaml_members
       end
     end
 
-        server_projects = server_team.respond_to?(:fake) ? [] : list_team_projects(server_team['id'])
-
-        @diff_hash[:add_projects][server_team] = yaml_projects - server_projects
-        @diff_hash[:remove_projects][server_team] = server_projects - yaml_projects
     def projects_diff
       if server_team.id.present?
+        diff_hash[:add_projects][server_team] = add_projects
+        diff_hash[:remove_projects][server_team] = remove_projects
       elsif !yaml_projects.empty?
         @diff_hash[:create_teams][server_team][:add_projects] = yaml_projects
       end
     end
 
-    def list_team_members(team_id)
-      @rollbar_api.list_team_members(team_id).map { |e| e['email'] }
+    def add_members
+      yaml_members.reject { |key, _e| server_members.keys.include?(key) }
     end
 
-    def list_team_projects(team_id)
-      @rollbar_api.list_team_projects(team_id).map { |e| e['name'] }
+    def remove_members
+      server_members.reject { |key, _e| yaml_members.keys.include?(key) }
+    end
+
+    def add_projects
+      yaml_projects.reject { |key, _e| server_projects.keys.include?(key) }
+    end
+
+    def remove_projects
+      server_projects.reject { |key, _e| yaml_projects.keys.include?(key) }
     end
 
     def create_server_team
@@ -59,10 +64,46 @@ module RollbarIntegration
       @yaml_team
     end
 
-    def map_users_to_emails
-      users = User.find_many(@yaml_team.members)
+    def yaml_members
+      return @yaml_members if @yaml_members.present?
+      users = User.find_many(yaml_team.members)
       @errors.push(*User.shift_errors) if User.errors.present?
-      users.values.map(&:email)
+      @yaml_members = Hash[users.values.map { |e| [e.email, e] }]
+    end
+
+    def yaml_projects
+      return @yaml_projects if @yaml_projects.present?
+      account_projects = Hash[
+        rollbar_api.list_account_projects.map { |p| [p['name'], p['id']] }]
+      @yaml_projects = Hashie::Mash.new(
+        Hash[
+          yaml_team.projects
+          .map { |e| [e, { id: account_projects[e], name: e }] }
+        ])
+    end
+
+    def server_projects
+      @server_projects ||= if server_team.respond_to?(:fake)
+                             []
+                           else
+                             Hash[
+                              rollbar_api
+                              .list_team_projects(server_team['id'])
+                              .map { |e| [e.name, e] }
+                             ]
+                           end
+    end
+
+    def server_members
+      @server_members ||= if server_team.respond_to?(:fake)
+                            []
+                          else
+                            Hash[
+                              rollbar_api
+                              .list_team_members(server_team['id'])
+                              .map { |e| [e.email, e] }
+                            ]
+                          end
     end
   end
 end
