68f953bb8a6f361e714472a2fa4425f0154f85f5
Mariusz Błaszczak <mariusz.blaszczak@netguru.co>
2015-09-24 15:14:30 +0200

    Update spec helper rollbar_api to adjust it to new methods

--- spec/support/rollbar_api.rb
+++ spec/support/rollbar_api.rb
@@ -4,22 +4,40 @@ RSpec.shared_context 'rollbar_api' do
       allow(api).to receive(:list_teams) { existing_teams }
       allow(api).to receive(:list_team_members) { existing_members }
       allow(api).to receive(:list_team_projects) { existing_projects }
-      allow(api).to receive(:remove_project) do |project_name, team|
-        team.projects.delete_if { |r| r.name == project_name }
+      allow(api).to receive(:remove_project_from_team) do |project_id, _team_id|
+        project = all_projects.find { |p| p.id == project_id }
+        team.projects.delete(project.name)
       end
-      allow(api).to receive(:add_project) do |project_name, team|
-        team.projects.push(Hashie::Mash.new name: project_name)
+      allow(api).to receive(:add_project_to_team) do |project_id, team_id|
+        project = all_projects.find { |p| p.id == project_id }
+        if team_id == 1
+          team.projects[project.name] = project
+        else
+          new_team.projects[project.name] = project
+        end
       end
-      allow(api).to receive(:add_member) do |member_username, team|
-        team.members.push(Hashie::Mash.new username: member_username)
+      allow(api).to receive(:invite_member_to_team) do |member_email, team_id|
+        member = all_members.find { |m| m.email == member_email }
+        if team_id == 1
+          team.members[member_email] = member
+        else
+          new_team.members[member_email] = member
+        end
       end
-      allow(api).to receive(:remove_member) do |member_username, team|
-        team.members.delete_if { |m| m.username == member_username }
+      allow(api).to receive(:remove_member_from_team) do |member_id, _team_id|
+        member = all_members.find { |m| m.id == member_id }
+        team.members.delete(member.email)
       end
       allow(api).to receive(:create_team) do
         existing_teams.push(new_team)
-      end.and_yield(new_team)
+      end.and_yield(created_new_team)
+      allow(api).to receive(:list_account_projects) { all_projects }
     end
+  end
+
+  let(:created_new_team) do
+    new_team.id = 2
+    new_team
   end
 
   let(:team1) do
@@ -28,6 +46,17 @@ RSpec.shared_context 'rollbar_api' do
       id: 1,
       account_id: 1,
     )
+  end
+
+  let(:team2) do
+    Hashie::Mash.new(
+      name: 'team2',
+      id: 2,
+    )
+  end
+
+  let(:new_team) do
+    Hashie::Mash.new name: 'new team'
   end
 
   let(:member1) do
@@ -86,4 +115,6 @@ RSpec.shared_context 'rollbar_api' do
   let(:existing_teams) { [team1] }
   let(:existing_members) { [member1, member2] }
   let(:existing_projects) { [project1, project2] }
+  let(:all_projects) { [project1, project2, project3] }
+  let(:all_members) { [member1, member2, member3, member6] }
 end
