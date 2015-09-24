7ce562bd7b8156b82d492ecfa03a3ef2dec1be33
Mariusz Błaszczak <mariusz.blaszczak@netguru.co>
2015-09-24 15:10:37 +0200

    Method create_team should be able to receive block

--- app/models/rollbar_integration/api.rb
+++ app/models/rollbar_integration/api.rb
@@ -23,7 +23,8 @@ module RollbarIntegration
 
     def create_team(name, access_level = 'standard')
       options = { body: { name: name, access_level: access_level } }
-      client.post('/api/1/teams', options)
+      team = client.post('/api/1/teams', options)
+      yield team
     end
 
     def create_project(name)
