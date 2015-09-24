f7b9ccfeabc9ed7226cab0d000f30845601510d4
Mariusz Błaszczak <mariusz.blaszczak@netguru.co>
2015-09-24 15:11:30 +0200

    Add new methods for API rollbar for necessary for syncing

--- app/models/rollbar_integration/api.rb
+++ app/models/rollbar_integration/api.rb
@@ -16,9 +16,17 @@ module RollbarIntegration
         .map { |member| get_member(member['user_id']) }
     end
 
+    def list_account_projects
+      client.get('/api/1/projects')
+    end
+
     def list_team_projects(team_id)
       client.get_all_pages("/api/1/team/#{team_id}/projects")
         .map { |project| get_project(project['project_id']) }
+    end
+
+    def list_team_invities(team_id)
+      client.get("/api/1/team/#{team_id}/invites")
     end
 
     def create_team(name, access_level = 'standard')
@@ -36,8 +44,25 @@ module RollbarIntegration
       client.put("/api/1/team/#{team_id}/project/#{project_id}")
     end
 
+    def invite_member_to_team(email, team_id)
+      options = { body: { email: email } }
+      client.post("/api/1/team/#{team_id}/invites", options)
+    end
+
+    def remove_member_from_team(user_id, team_id)
+      client.delete("/api/1/team/#{team_id}/user/#{user_id}")
+    end
+
     def remove_team(team_id)
       client.delete("/api/1/team/#{team_id}")
+    end
+
+    def remove_project_from_team(project_id, team_id)
+      client.delete("/api/1/team/#{team_id}/project/#{project_id}")
+    end
+
+    def cancel_invitation(invitation_id)
+      client.delete("/api/1/invite/#{invitation_id}")
     end
 
     private
