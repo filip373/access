2121970ad5014324fefc21dd8fda914e4b07a3a0
Mateusz Stebnicki <mateusz.stebnicki@netguru.pl>
2015-09-25 16:05:44 +0200

    use data guru for gh teams

--- app/models/github_integration/teams.rb
+++ app/models/github_integration/teams.rb
@@ -1,26 +1,18 @@
 module GithubIntegration
   class Teams
     def self.all
-      raw_data.map do |team_name, team_data|
+      raw_data.map do |team|
         Team.new(
-          team_name,
-          team_data.members || [],
-          team_data.repos || [],
-          team_data.permission || default_permission,
+          team.id,
+          team.members,
+          team.repos,
+          team.permission,
         )
       end
     end
 
     def self.raw_data
-      Storage.data.github_teams
-    end
-
-    def self.teams_repo_path
-      "#{Rails.root}/tmp/permissions/github_teams"
-    end
-
-    def self.default_permission
-      'push'
+      DataGuru::Client.new.github_teams
     end
   end
 end
