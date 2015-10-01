3415f0169822a98200c62db9425d39231ba21cfe
Mariusz Błaszczak <mariusz.blaszczak@netguru.co>
2015-10-01 09:10:56 +0200

    Replace storage with DataGuru in rollbar_teams

--- app/models/rollbar_integration/teams.rb
+++ app/models/rollbar_integration/teams.rb
@@ -1,21 +1,17 @@
 module RollbarIntegration
   class Teams
     def self.all
-      raw_data.map do |_team_name, team_data|
+      raw_data.map do |team|
         Team.new(
-          team_data.name,
-          team_data.members || [],
-          team_data.projects || [],
+          team.name,
+          team.members,
+          team.projects,
         )
       end
     end
 
     def self.raw_data
-      Storage.data.rollbar_teams || []
-    end
-
-    def self.teams_repo_path
-      "#{Rails.root}/tmp/permissions/rollbar_teams"
+      DataGuru::Client.new.rollbar_teams
     end
   end
 end
