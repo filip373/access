efcfe8e18f85972f19fd1996b8600023f275f1d6
Adam Nowak <adam.nowak@netguru.co>
2015-10-02 08:30:27 +0200

    refresh permissions data on start

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,9 +2,11 @@ class MainController < ApplicationController
   include ::GithubApi
   before_action :check_permissions
 
-  expose(:validation_errors) { DataGuru::Client.new.errors }
+  expose(:data_guru) { DataGuru::Client.new }
+  expose(:validation_errors) { data_guru.errors }
 
   def check_permissions
+    data_guru.refresh
     gh_api.client.patch_request("/orgs/#{gh_api.client.org}")
     rescue Github::Error::NotFound
       render 'main/unauthorized'
