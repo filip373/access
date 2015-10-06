1570aa5d37078c7af57f4dbe80ded4b5c76eda93
Marcin Siepetowski <marcin.siepetowski@netguru.pl>
2015-10-06 17:17:22 +0200

    add user repository

---
+++ app/repositories/user_repository.rb
@@ -0,0 +1,48 @@
+class UserRepository
+  attr_accessor :users_data, :errors
+
+  def initialize(users_data = nil)
+    @users_data = users_data || DataGuru::Client.new.users
+    @errors = []
+  end
+
+  def find(name)
+    user =
+      if name.include?('/')
+        nick = name.split('/').last
+        users_data.find { |u| u.id == nick }
+      else
+        users_data.find { |u| u.id == name }
+      end
+    user || fail(UserError,
+                 "Unknown user #{name}. It's not in directory users or it is in wrong directory")
+  end
+
+  def find_by_email(email)
+    user = users_data.find do |u|
+      u.emails.include?(email)
+    end
+    user || fail(UserError, "User with email: #{email} does not exist.")
+  end
+
+  def find_many(names)
+    users = names.map do |n|
+      begin
+        user = find(n)
+      rescue StandardError => e
+        errors.push(e.to_s)
+        Rollbar.error(e)
+      else
+        [user.id, user]
+      end
+    end
+    hash2 = users.compact.each_with_object({}) do |user, memo|
+      next unless user.is_a?(Array)
+      memo[user.first] = user.last
+    end
+  end
+
+  def list_company_users
+    users_data.reject(&:external)
+  end
+end
