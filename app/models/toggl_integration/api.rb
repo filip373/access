0b85e13e6139d79e5e36cf3d4f3b39299c493251
Jacek Adamek <jacek.adamek@netguru.pl>
2015-10-06 13:47:12 +0200

    Adding Api class for toggl integration

---
+++ app/models/toggl_integration/api.rb
@@ -0,0 +1,35 @@
+module TogglIntegration
+  class Api
+    attr_reader :toggl_client
+
+    def initialize(token: AppConfig.toggl_token)
+      @toggl_client = TogglV8::API.new(token)
+    end
+
+    def list_teams
+      toggl_client.projects(workspace['id'])
+    end
+
+    def list_all_members
+      toggl_client.workspace_users(workspace['id'])
+    end
+
+    def list_team_members(team_id)
+      project_users = toggl_client.get_project_users(team_id)
+      project_users.map { |project_user| member_by_uid(project_user['uid']) }
+    end
+
+    private
+
+    def workspace
+      @workspace ||=
+        toggl_client.workspaces.find { |workspace| workspace['name'] == AppConfig.company }
+    end
+
+    def member_by_uid(member_uid)
+      @all_members ||=
+        list_all_members.reduce({},) { |acc, member| acc[member['uid'].to_i] = member; acc }
+      @all_members[member_uid.to_i]
+    end
+  end
+end
