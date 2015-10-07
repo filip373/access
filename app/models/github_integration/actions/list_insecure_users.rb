bef080c7ffd3d35fb3f055900830c5816a19df5d
Adam Nowak <adam.nowak@netguru.co>
2015-10-07 16:50:06 +0200

    refactor insecure users class

--- app/models/github_integration/actions/list_insecure_users.rb
+++ app/models/github_integration/actions/list_insecure_users.rb
@@ -16,21 +16,31 @@ module GithubIntegration
       private
 
       def convert_members_to_users
-        gh_members.map do |m|
+        gh_members.map do |gh_member|
           dg_user = users.find do |u|
-            u.github.to_s.downcase == m['login'].downcase
+            u.github.to_s.downcase == gh_member['login'].downcase
           end
-          teams = github_teams.select { |t| t.members.map(&:downcase).any? { |m| m.match(dg_user.id.downcase) } }
-          u = InsecureUser.new(github: dg_user.id,
-                               name: dg_user.name,
-                               emails: dg_user.emails,
-                              )
-          u.html_url = m['html_url']
-          u.github_teams = teams.map(&:id).join(', ')
-          u
+          build_user(dg_user, gh_member)
         end
       end
+
+      def build_user(dg_user, gh_member)
+        u = InsecureUser.new(github: dg_user.id,
+                             name: dg_user.name,
+                             emails: dg_user.emails,
+                            )
+        u.html_url = gh_member['html_url']
+        u.github_teams = select_teams_with_user(dg_user.id)
+        u
+      end
+
+      def select_teams_with_user(user_id)
+        github_teams.select do |t|
+          t.members.map(&:downcase).any? { |m| m.match(user_id.downcase) }
+        end.map(&:id).join(', ')
+      end
     end
+
     class InsecureUser < User
       attr_accessor :html_url, :github_teams
     end
