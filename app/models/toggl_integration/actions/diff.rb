6bb1f7e66e363d7cbd5c2754d9f93a49d09e880f
Jacek Adamek <jacek.adamek@netguru.pl>
2015-10-07 13:59:50 +0200

    Adding Diff class which calculate differences between local and server teams

---
+++ app/models/toggl_integration/actions/diff.rb
@@ -0,0 +1,72 @@
+module TogglIntegration
+  module Actions
+    class Diff
+      attr_reader :local_teams
+
+      def initialize(local_teams, toggl_api)
+        @local_teams = local_teams
+        @toggl_api = toggl_api
+      end
+
+      def call
+        reset_diff_hash
+        diff_teams
+        diff_hash
+      end
+
+      def diff_hash
+        @diff_hash ||= {
+          create_teams: {},
+          add_members: {},
+          remove_members: {},
+        }
+      end
+
+      private
+
+      # This method is time expensive (calls remote API).
+      def server_teams
+        @server_teams ||= @toggl_api.list_teams.map do |team|
+          Team.from_api_request(toggl_api, team)
+        end
+      end
+
+      def diff_teams
+        local_teams.each do |local_team|
+          server_team = server_teams.find { |st| st.name.downcase == local_team.name.downcase }
+          if server_team
+            diff_teams_members(local_team, server_team)
+            server_teams.delete(server_team)
+          else
+            diff_array(:create_teams, local_team.name) << local_team
+          end
+        end
+      end
+
+      def diff_teams_members(local_team, server_team)
+        return unless local_team.name == server_team.name
+
+        local_team.members.each do |local_member|
+          server_member = server_team.members.find { |sm| sm.downcase == local_member.downcase }
+          if server_member
+            server_team.members.delete(server_member)
+          else
+            diff_array(:add_members, local_team.name) << local_member
+          end
+        end
+
+        unless server_team.members.empty?
+          diff_array(:remove_members, server_team.name).concat server_team.members
+        end
+      end
+
+      def diff_array(group, team_name)
+        diff_hash[group][team_name] ||= []
+      end
+
+      def reset_diff_hash
+        @diff_hash = nil
+      end
+    end
+  end
+end
---
+++ spec/models/toggl_integration/actions/diff_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+
+describe TogglIntegration::Actions::Diff do
+  describe '#call' do
+    let(:team1) { TogglIntegration::Team.new('team1', ['john.doe', 'james.bond'], ['team1']) }
+    let(:team2) { TogglIntegration::Team.new('team2', ['john.doe', 'james.bond'], ['team2']) }
+    let(:team3) { TogglIntegration::Team.new('team3', ['john.doe', 'james.bond'], ['team3']) }
+    let(:team4) { TogglIntegration::Team.new('team4', ['john.doe'], ['team4']) }
+
+    let(:local_teams) { [team1, team2, team3, team4] }
+    let(:server_teams) do
+      [
+        TogglIntegration::Team.new('team1', ['john.doe'], ['team1']),
+        TogglIntegration::Team.new('team2', ['john.doe', 'james.bond', 'john.wayne'], ['team2']),
+      ]
+    end
+
+    let(:toggl_api) { double(:toggl_api) }
+    let(:diff) do
+      diff = described_class.new(local_teams, toggl_api)
+      allow(diff).to receive(:server_teams) { server_teams }
+      diff
+    end
+    let(:diff_result) { diff.call }
+
+    it 'returns hash with differences' do
+      expect(diff_result).to be_a(Hash)
+    end
+
+    it 'returns list of members to add' do
+      expect(diff_result[:add_members].size).to eq 1
+      expect(diff_result[:add_members]['team1']).to eq ['james.bond']
+    end
+
+    it 'returns list of members to remove' do
+      expect(diff_result[:remove_members].size).to eq 1
+      expect(diff_result[:remove_members]['team2']).to eq ['john.wayne']
+    end
+
+    it 'returns list of teams to create' do
+      expect(diff_result[:create_teams].size).to eq 2
+      expect(diff_result[:create_teams]['team3']).to eq [team3]
+      expect(diff_result[:create_teams]['team4']).to eq [team4]
+    end
+  end
+end
