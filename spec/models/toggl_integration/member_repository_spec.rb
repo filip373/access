da065f88774a3e320e7e595a79863a288b6cd68f
Jacek Adamek <jacek.adamek@netguru.pl>
2015-10-09 16:02:42 +0200

    Adding specs for member repository

---
+++ spec/models/toggl_integration/member_repository_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+
+describe TogglIntegration::MemberRepository do
+  include_context 'toggl_api'
+
+  let(:repository) { described_class.build_from_toggl_api(toggl_api) }
+
+  describe '#all' do
+    it 'returns all members' do
+      expect(repository.all.count).to eq 3
+    end
+  end
+
+  describe '#find_by_emails' do
+    context 'member exists' do
+      it 'returns member with given email' do
+        member = repository.find_by_emails(member1['email'])
+        expect(member.emails).to include member1['email']
+        expect(member.toggl_id).to eq member1['uid'].to_i
+      end
+    end
+
+    context 'member does not exist' do
+      it 'returns nil' do
+        member = repository.find_by_emails('asakdkf4848@gmail.com')
+        expect(member).to be_nil
+      end
+    end
+  end
+
+  describe '#find_by_toggl_id' do
+    context 'member exists' do
+      it 'returns member with given toggl user id' do
+        member = repository.find_by_toggl_id(member1['uid'])
+        expect(member.emails).to include member1['email']
+        expect(member.toggl_id).to eq member1['uid'].to_i
+      end
+    end
+
+    context 'member does not exist' do
+      it 'returns nil' do
+        member = repository.find_by_toggl_id('13')
+        expect(member).to be_nil
+      end
+    end
+  end
+end
