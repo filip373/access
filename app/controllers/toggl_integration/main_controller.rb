48443ff7d9817b09677577036c93c59079642a90
Jacek Adamek <jacek.adamek@netguru.pl>
2015-10-09 16:05:37 +0200

    Applying changes in Diff class

--- app/controllers/toggl_integration/main_controller.rb
+++ app/controllers/toggl_integration/main_controller.rb
@@ -3,7 +3,7 @@ module TogglIntegration
     CACHE_KEY_NAME = 'toggl_calculated_diff'.freeze
 
     expose(:validation_errors) { data_guru.errors }
-    expose(:diff_errors) { @diff.errors }
+    expose(:diff_errors) { @diff.errors.uniq }
     expose(:toggl_log) { Actions::Log.new(calculated_diff).call }
     expose(:missing_teams) { calculated_diff[:missing_teams] }
     expose(:toggl_api) { Api.new(AppConfig.toggl_token, AppConfig.company) }
@@ -19,6 +19,7 @@ module TogglIntegration
     end
 
     def cleanup_teams
+      # Action::CleanupTeams.new(missing_teams, toggl_api).call
     end
 
     private
@@ -29,7 +30,10 @@ module TogglIntegration
 
     def calculated_diff
       Rails.cache.fetch CACHE_KEY_NAME do
-        @diff ||= Actions::Diff.new(expected_teams, current_teams)
+        @diff ||= Actions::Diff.new(expected_teams,
+                                    current_teams,
+                                    expected_memebers_repository,
+                                    current_members_repository)
         @diff.call
       end
     end
@@ -39,7 +43,15 @@ module TogglIntegration
     end
 
     def expected_teams
-      TeamRepository.build_from_data_guru(data_guru).all
+      TeamRepository.build_from_data_guru(DataGuru.client.new).all
+    end
+
+    def current_members_repository
+      MemberRepository.build_from_toggl_api(toggl_api)
+    end
+
+    def expected_memebers_repository
+      MemberRepository.build_from_data_guru(DataGuru.client.new)
     end
   end
 end
