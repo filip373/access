10a6980210112ae6be8043d1243ae92c348d4373
Jacek Adamek <jacek.adamek@netguru.pl>
2015-10-09 16:04:32 +0200

    Changing class finding differences between expected and current teams

--- app/models/toggl_integration/actions/diff.rb
+++ app/models/toggl_integration/actions/diff.rb
@@ -1,11 +1,13 @@
 module TogglIntegration
   module Actions
     class Diff
-      attr_reader :local_teams, :current_teams, :errors
+      attr_reader :local_teams, :current_teams, :errors, :local_members_repo, :toggl_members_repo
 
-      def initialize(local_teams, current_teams)
+      def initialize(local_teams, current_teams, local_members_repo, toggl_members_repo)
         @local_teams = local_teams
         @current_teams = current_teams
+        @local_members_repo = local_members_repo
+        @toggl_members_repo = toggl_members_repo
         @errors = []
       end
 
@@ -17,7 +19,7 @@ module TogglIntegration
 
       def diff_hash
         @diff_hash ||= {
-          create_teams: [],
+          create_teams: {},
           add_members: {},
           remove_members: {},
           missing_teams: [],
@@ -26,31 +28,67 @@ module TogglIntegration
 
       private
 
+      def toggl_members_from_repo_id(*repo_ids)
+        members = []
+        repo_ids.each do |repo_id|
+          repo_member = local_members_repo.find_by_repo_id(repo_id)
+          unless repo_member
+            @errors << "User #{repo_id} was not found in repository."
+            next
+          end
+          toggl_member = toggl_members_repo.find_by_emails(*repo_member.emails)
+          unless toggl_member
+            @errors << "User #{repo_id} has no account in Toggle app."
+            next
+          end
+          members << toggl_member
+        end
+        members
+      end
+
+      def toggl_members_from_email(*emails)
+        members = []
+        emails.each do |email|
+          toggl_member = toggl_members_repo.find_by_emails(email)
+          unless toggl_member
+            @errors << "User with email #{email} has no account in Toggle app."
+            next
+          end
+          members << toggl_member
+        end
+        members
+      end
+
       def diff_teams
+        server_teams = current_teams.dup
         local_teams.each do |local_team|
-          server_team = current_teams.find { |st| st.name.downcase == local_team.name.downcase }
+          server_team = server_teams.find { |st| st.name.downcase == local_team.name.downcase }
           if server_team
             diff_teams_members(local_team, server_team)
-            current_teams.delete(server_team)
+            server_teams.delete(server_team)
           else
-            diff_hash[:create_teams] << local_team
+            diff_hash_array(:create_teams, local_team).concat(
+              toggl_members_from_repo_id(*local_team.members))
           end
         end
-        diff_hash[:missing_teams].concat current_teams if current_teams.any?
+        diff_hash[:missing_teams].concat server_teams if server_teams.any?
       end
 
       def diff_teams_members(local_team, server_team)
         return unless local_team.name == server_team.name
-        local_team.members.each do |local_member|
-          server_member = server_team.members.find { |sm| sm.downcase == local_member.downcase }
+        server_team_members = toggl_members_from_email(*server_team.members)
+        local_team_members = toggl_members_from_repo_id(*local_team.members)
+
+        local_team_members.each do |local_member|
+          server_member = server_team_members.find { |sm| (sm.emails & local_member.emails).any? }
           if server_member
-            server_team.members.delete(server_member)
+            server_team_members.delete(server_member)
           else
             diff_hash_array(:add_members, server_team) << local_member
           end
         end
-        unless server_team.members.empty?
-          diff_hash_array(:remove_members, server_team).concat server_team.members
+        if server_team_members.any?
+          diff_hash_array(:remove_members, server_team).concat(server_team_members)
         end
       end
 
--- spec/models/toggl_integration/actions/diff_spec.rb
+++ spec/models/toggl_integration/actions/diff_spec.rb
@@ -2,51 +2,90 @@ require 'rails_helper'
 
 describe TogglIntegration::Actions::Diff do
   describe '#call' do
-    let(:team1) { TogglIntegration::Team.new('team1', ['john.doe', 'james.bond'], ['team1']) }
-    let(:team2) { TogglIntegration::Team.new('team2', ['john.doe', 'james.bond'], ['team2']) }
-    let(:team3) { TogglIntegration::Team.new('team3', ['john.doe', 'james.bond'], ['team3']) }
-    let(:team4) { TogglIntegration::Team.new('team4', ['john.doe'], ['team4']) }
-    let(:local_teams) { [team1, team2, team3, team4] }
+    let(:local_team1) { TogglIntegration::Team.new('team1', ['john.doe', 'james.bond'], ['team1']) }
+    let(:local_team2) { TogglIntegration::Team.new('team2', ['john.doe', 'james.bond'], ['team2']) }
+    let(:local_team3) { TogglIntegration::Team.new('team3', ['john.doe', 'james.bond'], ['team3']) }
+    let(:local_team4) { TogglIntegration::Team.new('team4', ['john.doe'], ['team4']) }
+    let(:local_team5) { TogglIntegration::Team.new('team5', ['lucky.luke', 'winnetou'], ['team5']) }
+    let(:local_teams) { [local_team1, local_team2, local_team3, local_team4, local_team5] }
 
-    let(:server_team1) { TogglIntegration::Team.new('team1', ['john.doe'], ['team1'], '1') }
+    let(:server_team1) do
+      TogglIntegration::Team.new('team1', ['john.doe@gmail.com'], ['team1'], '1')
+    end
     let(:server_team2) do
       TogglIntegration::Team.new(
-        'team2', ['john.doe', 'james.bond', 'john.wayne'], ['team2'], '2')
+        'team2',
+        ['john.doe@gmail.com', 'james.bond@gmail.com', 'john.wayne@gmail.com'],
+        ['team2'],
+        '2')
     end
-    let(:server_team5) do
+    let(:server_team6) do
       TogglIntegration::Team.new(
-        'team5', ['john.doe', 'james.bond', 'john.wayne'], ['team5'], '5')
+        'team6',
+        ['john.doe@gmail.com', 'james.bond@gmail.com', 'john.wayne@gmail.com'],
+        ['team6'],
+        '6')
     end
-    let(:server_teams) { [server_team1, server_team2, server_team5] }
+    let(:server_teams) { [server_team1, server_team2, server_team6] }
+
+    let(:toggl_members_repo) do
+      members = [
+        TogglIntegration::Member.new(emails: ['john.doe@gmail.com'], toggl_id: '1'),
+        TogglIntegration::Member.new(emails: ['john.wayne@gmail.com'], toggl_id: '2'),
+        TogglIntegration::Member.new(emails: ['james.bond@gmail.com'], toggl_id: '3'),
+      ]
+      TogglIntegration::MemberRepository.new(all: members)
+    end
+    let(:local_members_repo) do
+      members = [
+        TogglIntegration::Member.new(
+          emails: ['john.doe@gmail.com', 'john.doe@yahoo.com'], repo_id: 'john.doe'),
+        TogglIntegration::Member.new(emails: ['john.wayne@gmail.com'], repo_id: 'john.wayne'),
+        TogglIntegration::Member.new(emails: ['james.bond@gmail.com'], repo_id: 'james.bond'),
+        TogglIntegration::Member.new(emails: ['winnetou@gmail.com'], repo_id: 'winnetou'),
+      ]
+      TogglIntegration::MemberRepository.new(all: members)
+    end
 
     let(:diff) do
-      diff = described_class.new(local_teams, server_teams)
-      diff
+      described_class.new(local_teams, server_teams, local_members_repo, toggl_members_repo)
     end
-    let(:diff_result) { diff.call }
 
     it 'returns hash with differences' do
+      diff_result = diff.call
       expect(diff_result).to be_a(Hash)
     end
 
     it 'returns list of members to add' do
+      diff_result = diff.call
       expect(diff_result[:add_members].size).to eq 1
-
-      expect(diff_result[:add_members][server_team1]).to eq ['james.bond']
+      expect(diff_result[:add_members][server_team1][0].emails).to eq ['james.bond@gmail.com']
     end
 
     it 'returns list of members to remove' do
+      diff_result = diff.call
       expect(diff_result[:remove_members].size).to eq 1
-      expect(diff_result[:remove_members][server_team2]).to eq ['john.wayne']
+      expect(diff_result[:remove_members][server_team2][0].emails).to eq ['john.wayne@gmail.com']
     end
 
     it 'returns list of teams to create' do
-      expect(diff_result[:create_teams].size).to eq 2
-      expect(diff_result[:create_teams]).to eq [team3, team4]
+      diff_result = diff.call
+      expect(diff_result[:create_teams].size).to eq 3
+      expect(diff_result[:create_teams][local_team3]).not_to be_nil
+      expect(diff_result[:create_teams][local_team4]).not_to be_nil
+      expect(diff_result[:create_teams][local_team5]).not_to be_nil
     end
 
     it 'returns list of missing teams' do
-      expect(diff_result[:missing_teams]).to eq [server_team5]
+      diff_result = diff.call
+      expect(diff_result[:missing_teams]).to eq [server_team6]
+    end
+
+    it 'returns list of errors' do
+      diff.call
+      expect(diff.errors.count).to eq(2)
+      expect(diff.errors[0]).to include('User lucky.luke was not found')
+      expect(diff.errors[1]).to include('User winnetou has no account')
     end
   end
 end
