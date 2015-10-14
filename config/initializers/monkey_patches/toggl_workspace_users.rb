a892f3d16ad062b88fddc29a779d336bd5552316
Jacek Adamek <jacek.adamek@netguru.pl>
2015-10-14 16:48:37 +0200

    Extending gem with method which allows to update workspace user

--- config/initializers/monkey_patches/toggl_workspace_users.rb
+++ config/initializers/monkey_patches/toggl_workspace_users.rb
@@ -4,5 +4,9 @@ module TogglV8
     def workspace_users(workspace_id)
       get "workspaces/#{workspace_id}/workspace_users"
     end
+
+    def update_workspace_user(workspace_user_id, params)
+      put "workspace_users/#{workspace_user_id}", 'workspace_user' =>  params
+    end
   end
 end
