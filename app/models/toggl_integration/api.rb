fa7ebc986a4e56eecea4e77b6a2dc5fc3ce81209
Jacek Adamek <jacek.adamek@netguru.pl>
2015-10-14 17:16:52 +0200

    Replacing destructive actions with updates (deactivating teams or members instead of deleting them)

--- app/models/toggl_integration/api.rb
+++ app/models/toggl_integration/api.rb
@@ -10,35 +10,39 @@ module TogglIntegration
     end
 
     def list_teams
-      toggl_client.projects(workspace['id'])
+      toggl_client.projects(workspace['id'], active: true)
     end
 
     def list_all_members
-      toggl_client.workspace_users(workspace['id'])
+      @workspace_users ||= toggl_client.workspace_users(workspace['id'])
     end
 
     def list_team_members(team_id)
       list_projects_users(team_id).map do |project_user|
         member_by_uid(project_user.uid)
-      end
+      end.select { |member| !member['inactive'] }
     end
 
-    def delete_team(team_id)
-      toggl_client.delete_project(team_id)
+    def deactivate_team(team_id)
+      params = { 'active' => false }
+      toggl_client.update_project(team_id, params)
     end
 
     def add_member_to_team(member, team)
       user = list_projects_users(team.id).find { |user| user['uid'] == member.toggl_id }
-      unless user
+      if user
+        activate_member(member.toggl_id)
+      else
         params = { 'uid' => member.toggl_id, 'pid' => team.id }
         toggl_client.create_project_user(params)
       end
     end
 
-    def remove_member_from_team(member, team)
-      project_users = list_projects_users(team.id)
-      project_user_id = project_users.find { |pu| pu.uid == member.toggl_id }.id
-      toggl_client.delete_project_user(project_user_id)
+    def deactivate_member(member)
+      workspace_user = list_all_members.find { |m| m['uid'] == member.toggl_id }
+      return workspace_user if workspace_user['inactive']
+      params = { 'inactive' => true }
+      toggl_client.update_workspace_user(workspace_user['id'], params)
     end
 
     def create_team(team)
@@ -54,9 +58,9 @@ module TogglIntegration
     private
 
     def member_by_uid(member_uid)
-      @all_members ||=
-      list_all_members.each_with_object({}) { |member, acc| acc[member['uid'].to_i] = member }
-      @all_members[member_uid.to_i]
+      @members_by_uid ||=
+        list_all_members.each_with_object({}) { |member, acc| acc[member['uid'].to_i] = member }
+      @members_by_uid[member_uid.to_i]
     end
 
     def list_projects_users(team_id)
@@ -70,6 +74,13 @@ module TogglIntegration
           users << ProjectUser.new(pu['id'], pu['uid'])
         end
       end
+    end
+
+    def activate_member(uid)
+      workspace_user = list_all_members.find { |m| m['uid'] == uid }
+      return workspace_user unless workspace_user['inactive']
+      params = { 'inactive' => false }
+      toggl_client.update_workspace_user(workspace_user['id'], params)
     end
   end
 end
