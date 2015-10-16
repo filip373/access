1698861d3f3c42a7fba6efe37f20e3a51bfabca1
Mateusz Stebnicki <mateusz.stebnicki@netguru.pl>
2015-10-16 19:31:32 +0200

    add convenience methods for GH::Teams

--- app/models/github_integration/teams.rb
+++ app/models/github_integration/teams.rb
@@ -1,14 +1,15 @@
 module GithubIntegration
   class Teams
-    def self.all(raw_data)
-      raw_data.map do |team|
-        Team.new(
-          team.id,
-          team.members,
-          team.repos,
-          team.permission,
-        )
-      end
+    def self.all_from_storage(raw_data)
+      raw_data.map { |team| Team.from_storage(team) }
+    end
+
+    def self.all_from_api(client, gh_teams)
+      gh_teams.map { |team| Team.from_api_request(client, team) }
+    end
+
+    def self.names_and_ids(gh_teams)
+      gh_teams.each_with_object({}) { |team, hash| hash[team.name.downcase] = team.id }
     end
   end
 end
