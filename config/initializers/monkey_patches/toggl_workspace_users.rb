f4646acf0a61c877e763cfda96ff01a61760fe87
Jacek Adamek <jacek.adamek@netguru.pl>
2015-10-28 15:31:51 +0100

    Adding lacking method

--- config/initializers/monkey_patches/toggl_workspace_users.rb
+++ config/initializers/monkey_patches/toggl_workspace_users.rb
@@ -8,5 +8,9 @@ module TogglV8
     def update_workspace_user(workspace_user_id, params)
       put "workspace_users/#{workspace_user_id}", 'workspace_user' =>  params
     end
+
+    def invite_member(workspace_id, email)
+      post("workspaces/#{workspace_id}/invite", 'emails' =>  [email])[0]
+    end
   end
 end
