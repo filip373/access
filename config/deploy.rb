27db9b5320eda0f5ec5aa78bf9fb3f31ae32a892
Jacek Jakubik <jacek.jakubik@netguru.pl>
2015-10-29 16:42:50 +0100

    New deployment setup

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,6 +3,35 @@ require 'capistrano/upload'
 set :application, 'access'
 set :repo_url, 'git://github.com/netguru/access.git'
 set :deploy_to, ENV["DEPLOY_PATH"]
-set :linked_files, %w{config/sec_config.yml}
-set :linked_dirs, %w{log tmp vendor/bundle config/keys}
-set :passenger_restart_with_touch, true
+set :linked_files, %w{.dockerrc}
+set :copy_files, %w(config/sec_config.yml config/keys)
+
+namespace :deploy do
+  after :updated, :copy_secret_files do
+    on roles(:web) do
+      within release_path do
+        fetch(:copy_files).each do |file|
+          execute :cp, "-aR #{shared_path}/#{file} #{release_path}/#{file}"
+        end
+      end
+    end
+  end
+
+  after :copy_secret_files, :build do
+    on roles(:web) do
+      within release_path do
+        execute "docker/#{fetch(:stage)}/build"
+      end
+    end
+  end
+
+  task :restart do
+    on roles(:web) do
+      within release_path do
+        execute "docker/#{fetch(:stage)}/restart"
+      end
+    end
+  end
+
+  after :publishing, :restart
+end
