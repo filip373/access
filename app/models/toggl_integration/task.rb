115a042a0c8d33191c426f3157fff1339d13d25f
Mateusz Czerwiński <mateusz.czerwinski@netguru.pl>
2015-11-27 11:41:47 +0100

    add task model

---
+++ app/models/toggl_integration/task.rb
@@ -0,0 +1,30 @@
+module TogglIntegration
+  class Task
+    attr_reader :name, :pid
+
+    def initialize(name:, pid:)
+      @name = name
+      @pid = pid
+    end
+
+    def name?
+      !name.nil?
+    end
+
+    def project_id?
+      !pid.nil?
+    end
+
+    def ==(other)
+      return false unless self.class == other.class
+      name == other.name && pid == other.pid
+    end
+
+    def eql?(other)
+      self == other
+    end
+
+    def hash
+      [name, pid].hash
+  end
+end
