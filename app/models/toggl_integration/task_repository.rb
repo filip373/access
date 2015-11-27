11282389d1d71f43b13621ca8d0a41b410fc5a1f
Mateusz Czerwiński <mateusz.czerwinski@netguru.pl>
2015-11-27 11:42:13 +0100

    add task repository

---
+++ app/models/toggl_integration/task_repository.rb
@@ -0,0 +1,19 @@
+module TogglIntegration
+  class TaskRepository
+    attr_reader :all
+
+    def initialize(all: [])
+      @all = all
+    end
+
+    def self.build_from_toggl_api(toggl_api, project_id)
+      tasks = toggl_api.get_project_tasks(project_id).map do |task|
+        Task.new(
+          name: task['name'],
+          pid: task['pid'],
+        )
+      end
+      new(all: tasks)
+    end
+  end
+end
