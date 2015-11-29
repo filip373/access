bf8046c4aa132b6464a29b6ede30e44f59695de6
Mateusz Czerwiński <mateusz.czerwinski@netguru.pl>
2015-11-29 19:58:49 +0100

    change way of retrieving list of tasks for each project

--- app/models/toggl_integration/task_repository.rb
+++ app/models/toggl_integration/task_repository.rb
@@ -6,9 +6,11 @@ module TogglIntegration
       @all = all
     end
 
-    def self.build_from_toggl_api(toggl_api, project_id)
-      tasks = toggl_api.get_project_tasks(project_id).map do |task|
-        Task.new(name: task['name'], pid: task['pid'])
+    def self.build_from_toggl_api(toggl_api)
+      toggl_api.list_teams.each |team|
+        tasks = toggl_api.list_all_tasks(team.id).map do |task|
+          Task.new(name: task['name'], pid: task['pid'])
+        end
       end
       new(all: tasks)
     end
