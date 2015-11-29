c3e62e5fadea0173adcafae9c92bf482785c8f79
Mateusz Czerwiński <mateusz.czerwinski@netguru.pl>
2015-11-29 21:56:16 +0100

    add methods for differenting fields such as task

--- app/models/toggl_integration/actions/diff.rb
+++ app/models/toggl_integration/actions/diff.rb
@@ -1,19 +1,20 @@
 module TogglIntegration
   module Actions
     class Diff
-      attr_reader :local_teams, :current_teams, :errors, :toggl_members_repo, :user_repo
+      attr_reader :local_teams, :current_teams, :errors, :toggl_members_repo,
+        :user_repo, :toggl_tasks_repo
 
-      def initialize(local_teams, current_teams, user_repo, toggl_members_repo)
+      def initialize(local_teams, current_teams, user_repo, toggl_members_repo, toggl_tasks_repo)
         @local_teams = local_teams
         @current_teams = current_teams
         @toggl_members_repo = toggl_members_repo
+        @toggl_tasks_repo = toggl_tasks_repo
         @user_repo = user_repo
         @errors = []
       end
 
       def call
         reset_diff_hash
-        diff_teams
         find_members_without_permissions
         diff_hash
       end
@@ -25,6 +26,8 @@ module TogglIntegration
           remove_members: {},
           missing_teams: [],
           deactivate_members: Set.new,
+          create_tasks: {},
+          remove_tasks: {},
         }
       end
 
@@ -43,15 +46,33 @@ module TogglIntegration
         result
       end
 
-      def diff_teams
+      def normalize_tasks(*tasks)
+        result = []
+        tasks.each do |task|
+          if task.name.empty?
+            @errors << "Task #{task.id} has no name."
+            next
+          end
+          if task.pid.empty?
+            @errors << "Task #{task.id} has no project id."
+            next
+          end
+          result << task
+        end
+        result
+      end
+
+
         server_teams = current_teams.dup
         local_teams.each do |local_team|
           server_team = server_teams.find { |st| st.name.downcase == local_team.name.downcase }
           if server_team
             diff_teams_members(local_team, server_team)
+            diff_teams_tasks(local_team, server_team)
             server_teams.delete(server_team)
           else
             diff_hash_array(:create_teams, local_team).concat normalize_members(*local_team.members)
+            diff_hash_array(:create_tasks, local_team).concat normalize_tasks(*local_team.tasks)
           end
         end
         diff_hash[:missing_teams].concat server_teams if server_teams.any?
@@ -69,9 +90,24 @@ module TogglIntegration
             diff_hash_array(:add_members, server_team).concat normalize_members(local_member)
           end
         end
-        if server_team_members.any?
-          diff_hash_array(:remove_members, server_team).concat server_team_members
+        diff_hash_array(:remove_members, server_team)
+          .concat server_team_members if server_team_members.any?
+      end
+
+      def diff_teams_tasks(local_team, server_team)
+        return unless local_team.name == server_team.name
+        server_team_tasks = server_team.tasks.dup
+
+        local_team.tasks.each do |local_task|
+          server_task = server_team_tasks.find { |st| (st.tasks & local_task.tasks).any? }
+          if server_task
+            server_team_tasks.delete(server_task)
+          else
+            diff_hash_array(:add_tasks, server_team).concat normalize_tasks(local_task)
+          end
         end
+        diff_hash_array(:remove_tasks, server_team)
+          .concat server_team_tasks if server_team_tasks.any?
       end
 
       def diff_hash_array(group, team)
