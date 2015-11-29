284b41dcddf5f4fc562519a7efe25f4c1462b21c
Mateusz Czerwiński <mateusz.czerwinski@netguru.pl>
2015-11-29 22:29:20 +0100

    add logging methods for tasks

--- app/models/toggl_integration/actions/log.rb
+++ app/models/toggl_integration/actions/log.rb
@@ -17,8 +17,43 @@ module TogglIntegration
         log_add_members
         log_remove_members
         log_deactivate_members
+        log_creating_tasks
+        log_removing_tasks
         @log << 'There are no changes.' if @log.size == 0
         @log
+      end
+
+      def log_creating_tasks
+        @diff_hash[:create_tasks].each do |team, tasks|
+          @log << "[api] create task for #{team.name}"
+          tasks.each do |task|
+            log_create_task(task, team)
+          end
+        end
+      end
+
+      def log_create_task(task, team)
+        @log << if task.name?
+                  "[api] add task #{task.name} to team #{team.name}"
+                else
+                  "[api] could not add task #{task.name} to team #{team.name}"
+                end
+      end
+
+      def log_removing_tasks
+        @diff_hash[:remove_tasks].each do |team, tasks|
+          tasks.each do |task|
+            log_remove_task(task, team)
+          end
+        end
+      end
+
+      def log_remove_task(task, team)
+        @log << if task.name?
+                  "[api] remove task #{task.name} from team #{team.name}"
+                else
+                  "[api] could not remove task #{task.name} from team #{team.name}"
+                end
       end
 
       def log_creating_teams
@@ -57,7 +92,7 @@ module TogglIntegration
                   "[api] add member #{member.default_email} to team #{team.name}"
                 else
                   "[api] invite member #{member.default_email} to team #{team.name}"
-        end
+                end
       end
     end
   end
