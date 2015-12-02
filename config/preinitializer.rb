86f46f0b49e3c0dc0f9242400177adb3f1bd850a
Maciej Kalisz <maciej.d.kalisz@gmail.com>
2015-12-02 20:01:20 +0100

    Debug preinitializer for Circle

--- config/preinitializer.rb
+++ config/preinitializer.rb
@@ -5,6 +5,10 @@ config_path = File.expand_path('../config.yml', __FILE__)
 sec_config_path = File.expand_path('../sec_config.yml', __FILE__)
 
 pub_config = YAML.load(File.read(config_path))[Rails.env] || {}
-sec_config = YAML.load(File.read(sec_config_path))[Rails.env] || {}
+sec_config = if File.exist?(sec_config_path)
+               YAML.load(File.read(sec_config_path))[Rails.env] || {}
+             else
+               {}
+             end
 
 AppConfig = Konf.new(pub_config.deep_merge(sec_config))
