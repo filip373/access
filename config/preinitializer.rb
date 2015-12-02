1e144fd848f43643e3e2714fcee6d13e667bb7f6
Maciej Kalisz <maciej.d.kalisz@gmail.com>
2015-12-02 17:57:10 +0100

    Clean up preinitializer.rb

--- config/preinitializer.rb
+++ config/preinitializer.rb
@@ -1,11 +1,10 @@
 require 'active_support/core_ext'
 require 'konf'
-require 'pry'
 
 config_path = File.expand_path('../config.yml', __FILE__)
 sec_config_path = File.expand_path('../sec_config.yml', __FILE__)
 
-pub_config = YAML.load(ERB.new(File.read(config_path)).result)[Rails.env] || {}
-sec_config = File.exist?(sec_config_path) ? YAML.load(ERB.new(File.read(sec_config_path)).result)[Rails.env] : {}
+pub_config = YAML.load(File.read(config_path))[Rails.env] || {}
+sec_config = YAML.load(File.read(sec_config_path))[Rails.env] || {}
 
-AppConfig = Konf.new(pub_config.deep_merge(sec_config || {}))
+AppConfig = Konf.new(pub_config.deep_merge(sec_config))
