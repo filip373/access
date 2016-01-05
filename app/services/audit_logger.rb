cb2bdc29fe77f0c0f80a640285d46895fa204553
Szymon Szeliga <szymon.szeliga@netguru.co>
2016-01-05 08:36:56 +0100

    Remove severity from log

--- app/services/audit_logger.rb
+++ app/services/audit_logger.rb
@@ -1,8 +1,8 @@
 class AuditLogger < Logger
   def initialize(logdev, shift_age = 0, shift_size = nil)
     super(logdev, shift_age, shift_size)
-    self.formatter = proc do |severity, datetime, progname, msg|
-      "#{severity} #{datetime}: [#{progname}] #{msg}\n"
+    self.formatter = proc do |_severity, datetime, progname, msg|
+      "#{datetime}: [#{progname}] #{msg}\n"
     end
   end
 end
