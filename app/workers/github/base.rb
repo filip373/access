9a481cfb0b3a7b757de533c7df85d6bd8caf0cca
Marcin Siepetowski <marcin.siepetowski@netguru.pl>
2016-01-05 13:29:12 +0100

    add github diff worker

---
+++ app/workers/github/base.rb
@@ -0,0 +1,26 @@
+module GithubWorkers
+  class Base
+
+    private
+
+    def data_guru
+      @data_guru ||= DataGuru::Client.new
+    end
+
+    def expected_teams
+      @expected_teams ||= GithubIntegration::Team.all(data_guru.github_teams)
+    end
+
+    def user_repo
+      @user_repo ||= UserRepository.new(data_guru.users.all)
+    end
+
+    def gh_api
+      @gh_api ||= GithubIntegration::Api.new(@session_token, AppConfig.company)
+    end
+
+    def gh_teams
+      @gh_teams ||= gh_api.list_teams
+    end
+  end
+end
---
+++ app/workers/github/diff_worker.rb
@@ -0,0 +1,42 @@
+module GithubWorkers
+  class DiffWorker < Base
+    include Sidekiq::Worker
+
+    def perform(session_token)
+      @session_token = session_token
+      set_performing_flag
+      calculated_diff(expected_teams, gh_teams, gh_api, user_repo)
+      calc_diff_errors
+      unset_performing_flag
+    end
+
+    private
+
+    def calculated_diff(expected_teams, gh_teams, gh_api, user_repo)
+      Rails.cache.fetch('github_calculated_diff') do
+        @diff ||= GithubIntegration::Actions::Diff.new(expected_teams, gh_teams, gh_api, user_repo)
+        @diff.now!
+      end
+    end
+
+    def calc_diff_errors
+      Rails.cache.fetch('github_calculated_errors') do
+        @diff_errors = @diff.errors
+      end
+    end
+
+    def set_performing_flag
+      Rails.cache.delete('github_performing_diff')
+      Rails.cache.fetch('github_performing_diff') do
+        true
+      end
+    end
+
+    def unset_performing_flag
+      Rails.cache.delete('github_performing_diff')
+      Rails.cache.fetch('github_performing_diff') do
+        false
+      end
+    end
+  end
+end
