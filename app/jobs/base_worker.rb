5fac6928124bd37a9cdb594f34ac951d23d5adfd
Marcin Siepetowski <marcin.siepetowski@netguru.pl>
2016-01-14 16:02:56 +0100

    add methods to base worker

---
+++ app/jobs/base_worker.rb
@@ -0,0 +1,23 @@
+class BaseWorker < ActiveJob::Base
+  private
+
+  def data_guru
+    @data_guru ||= DataGuru::Client.new
+  end
+
+  def user_repo
+    @user_repo ||= UserRepository.new(data_guru.members.all)
+  end
+
+  def api
+    raise NotImplementedError
+  end
+
+  def api_teams
+    raise NotImplementedError
+  end
+
+  def expected_teams
+    raise NotImplementedError
+  end
+end
