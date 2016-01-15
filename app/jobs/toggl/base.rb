eb017ad85fbc1a8ff43a8476df3dab3d2214ca8f
Marcin Siepetowski <marcin.siepetowski@netguru.pl>
2016-01-15 11:29:55 +0100

    refactor toggl workers

--- app/jobs/toggl/base.rb
+++ app/jobs/toggl/base.rb
@@ -1,23 +1,13 @@
 module TogglWorkers
-  class Base < ActiveJob::Base
+  class Base < BaseWorker
     private
 
-    def data_guru
-      @data_guru ||= DataGuru::Client.new
-    end
-
-    def user_repo
-      @user_repo ||= UserRepository.new(data_guru.members.all)
-    end
-
     def current_members_repository
-      @current_members_repository ||= TogglIntegration::MemberRepository.build_from_toggl_api(
-        toggl_api)
+      @current_members_repository ||= TogglIntegration::MemberRepository.build_from_toggl_api(api)
     end
 
     def current_tasks_repository
-      @current_tasks_repository ||= TogglIntegration::TaskRepository.build_from_toggl_api(
-        toggl_api)
+      @current_tasks_repository ||= TogglIntegration::TaskRepository.build_from_toggl_api(api)
     end
 
     def expected_teams
@@ -27,14 +17,14 @@ module TogglWorkers
         current_members_repository).all
     end
 
-    def current_teams
-      @current_teams ||= TogglIntegration::TeamRepository.build_from_toggl_api(
-        toggl_api,
+    def api_teams
+      @api_teams ||= TogglIntegration::TeamRepository.build_from_toggl_api(
+        api,
         user_repo).all
     end
 
-    def toggl_api
-      @toggl_api ||= TogglIntegration::Api.new(AppConfig.toggl_token, AppConfig.company)
+    def api
+      @api ||= TogglIntegration::Api.new(AppConfig.toggl_token, AppConfig.company)
     end
   end
 end
