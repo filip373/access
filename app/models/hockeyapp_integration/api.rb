201bdb61b4d39c863068caec0cdf6309f00e5069
Marcin Siepetowski <marcin.siepetowski@netguru.pl>
2016-01-15 17:03:27 +0100

    add api calls for actions

---
+++ app/models/hockeyapp_integration/api.rb
@@ -0,0 +1,45 @@
+module HockeyAppIntegration
+  class Api
+    attr_accessor :client
+
+    def initialize
+      @client = Client.new
+    end
+
+    def add_team_to_app(app_id, team_id)
+      client.put("/apps/#{app_id}/app_teams/#{team_id}")
+    end
+
+    def create_app(options)
+      client.post('/apps/new', options)
+    end
+
+    def invite_user_to_app(app_id, user_email)
+      client.post("/apps/#{app_id}/app_users", email: user_email)
+    end
+
+    def list_apps
+      client.get('/apps')
+    end
+
+    def list_app_teams(app_id)
+      client.get("/apps/#{app_id}/app_teams")
+    end
+
+    def list_app_users(app_id)
+      client.get("/apps/#{app_id}/app_users")
+    end
+
+    def list_teams
+      client.get('/teams')
+    end
+
+    def remove_team_from_app(app_id, team_id)
+      client.delete("/apps/#{app_id}/app_teams/#{team_id}")
+    end
+
+    def remove_user_from_app(app_id, user_id)
+      client.delete("/apps/#{app_id}/app_users/#{user_id}")
+    end
+  end
+end
