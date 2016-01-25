e878847447946e729f29f8d8458c2a12967f395b
Marcin Siepetowski <marcin.siepetowski@netguru.pl>
2016-01-25 16:21:21 +0100

    use presenter instead of multiple exposures

--- app/controllers/hockey_app_integration/main_controller.rb
+++ app/controllers/hockey_app_integration/main_controller.rb
@@ -1,23 +1,18 @@
 module HockeyAppIntegration
   class MainController < ApplicationController
-    expose(:validations_errors) { data_guru.errors }
-    expose(:missing_apps_in_api) { calculated_diff[:missing_api_apps] }
-    expose(:missing_apps_in_dg) { calculated_diff[:missing_dg_apps] }
-    expose(:hockeyapp_log) { HockeyAppIntegration::Actions::Log.new(calculated_diff).now! }
     expose(:expected_apps) do
       HockeyAppIntegration::App.all_from_dataguru(data_guru.hockeyapp_apps.all)
     end
     expose(:hockeyapp_apps) do
-      HockeyAppIntegration::App.all_from_api(hockeyapp_api)
+      HockeyAppIntegration::App.all_from_api(HockeyAppIntegration::Api.new)
     end
-    expose(:hockeyapp_api) { HockeyAppIntegration::Api.new }
-    expose(:user_repo) { UserRepository.new(data_guru.members.all) }
 
     def calculate_diff
       redirect_to hockeyapp_show_diff_path
     end
 
     def show_diff
+      render locals: { presenter: ::Presenters::HockeyAppPresenter.new(calculated_diff, data_guru) }
     end
 
     def refresh_cache
