d743dbb08e8a7cc825d8c0fcea09450ff740042e
Marcin Siepetowski <marcin.siepetowski@netguru.pl>
2016-01-25 16:20:56 +0100

    add presenter for hockeyapp

---
+++ app/presenters/hockey_app_presenter.rb
@@ -0,0 +1,26 @@
+module Presenters
+  class HockeyAppPresenter
+    attr_reader :diff, :data_guru
+
+    def initialize(diff, data_guru)
+      @diff = diff
+      @data_guru = data_guru
+    end
+
+    def missing_api_apps
+      diff[:missing_api_apps]
+    end
+
+    def missing_dg_apps
+      diff[:missing_dg_apps]
+    end
+
+    def log
+      HockeyAppIntegration::Actions::Log.new(diff).now!
+    end
+
+    def validation_errors
+      data_guru.errors
+    end
+  end
+end
