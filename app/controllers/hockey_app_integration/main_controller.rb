652ee4464468d8420bf4edd9cbef0f6fce3fbdca
Marcin Siepetowski <marcin.siepetowski@netguru.pl>
2016-01-26 09:15:03 +0100

    dont use it under module

--- app/controllers/hockey_app_integration/main_controller.rb
+++ app/controllers/hockey_app_integration/main_controller.rb
@@ -12,7 +12,7 @@ module HockeyAppIntegration
     end
 
     def show_diff
-      render locals: { presenter: ::Presenters::HockeyAppPresenter.new(calculated_diff, data_guru) }
+      render locals: { presenter: ::HockeyAppPresenter.new(calculated_diff, data_guru) }
     end
 
     def refresh_cache
