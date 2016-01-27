f3db514c2fda3f92fa6fe060478e12316b9c6091
Szymon Szeliga <szymon.szeliga@netguru.co>
2016-01-27 14:08:33 +0100

    Change ivar to private reader

--- app/models/jira_integration/api.rb
+++ app/models/jira_integration/api.rb
@@ -1,26 +1,24 @@
 module JiraIntegration
   class Api
-    def initialize(jira_client)
-      @jira_client = jira_client
-    end
+    attr_private_initialize :jira_client
 
     def user(name)
-      @jira_client.User.find(name)
+      jira_client.User.find(name)
     end
 
     def projects
-      @jira_client.Project.all
+      jira_client.Project.all
     end
 
     def roles_for(project_key)
-      JSON.parse(@jira_client.get("/rest/api/2/project/#{project_key}/role").body)
+      JSON.parse(jira_client.get("/rest/api/2/project/#{project_key}/role").body)
     rescue ::JIRA::HTTPError
       Rollbar.info("There is no JIRA project with key #{project_key}")
       {}
     end
 
     def role_members(link)
-      JSON.parse(@jira_client.get(link).body)
+      JSON.parse(jira_client.get(link).body)
     end
   end
 end
