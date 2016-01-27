1a7b0cf81c9d3c6bedc1daa99cd1611cb2b36d32
Szymon Szeliga <szymon.szeliga@netguru.co>
2016-01-27 11:01:19 +0100

    AI-4 Add jira-ruby adapter

---
+++ app/models/jira_integration/api.rb
@@ -0,0 +1,26 @@
+module JiraIntegration
+  class Api
+    def initialize(jira_client)
+      @jira_client = jira_client
+    end
+
+    def user(name)
+      @jira_client.User.find(name)
+    end
+
+    def projects
+      @jira_client.Project.all
+    end
+
+    def roles_for(project_key)
+      JSON.parse(@jira_client.get("/rest/api/2/project/#{project_key}/role").body)
+    rescue ::JIRA::HTTPError
+      Rollbar.info("There is no JIRA project with key #{project_key}")
+      {}
+    end
+
+    def role_members(link)
+      JSON.parse(@jira_client.get(link).body)
+    end
+  end
+end
