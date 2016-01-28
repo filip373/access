40aa8600a680b0d26c1a8247e2299b875f5712aa
Marcin Siepetowski <marcin.siepetowski@netguru.pl>
2016-01-28 10:56:16 +0100

    move id to role to helper class

--- app/helpers/hockey_app_integration/main_helper.rb
+++ app/helpers/hockey_app_integration/main_helper.rb
@@ -1,7 +1,23 @@
 module HockeyAppIntegration
   module MainHelper
+    ROLES_HASH = {
+      owners: 0,
+      developers: 1,
+      members: 2,
+      testers: 3,
+    }.freeze
+    ID_ROLES_HASH = ROLES_HASH.invert.freeze
+
     def app_link(app_id)
       "https://rink.hockeyapp.net/manage/apps/#{app_id}"
+    end
+
+    def self.role_to_id(role)
+      ROLES_HASH[role]
+    end
+
+    def self.id_to_role(role)
+      ID_ROLES_HASH[role]
     end
   end
 end
